73ab5cebd12e57ac9c2cbb0fb9384a495be751e8
Jeff Trentacoste <jeff@blackcape.io>
2024-07-25 22:59:26 -0400

    Fixed a type that affected logger hierarchy.

--- src/oms_sensemaking/config.py
+++ src/oms_sensemaking/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class LogConfig(BaseSettings):
     """Logging configuration to be set for the server"""
-    logger_name: str = 'oms_sensemaker'
+    logger_name: str = 'oms_sensemaking'
     log_format: str = '{asctime:<20s}{levelname:<8s}{name} {message}'
     log_level: str = 'DEBUG'
 
