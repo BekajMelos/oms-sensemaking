2b74dcf5b72f019094a871d9d0561b3475624f55
charlie.beall <charlie.beall@blackcape.io>
2024-09-20 17:00:57 -0400

    Updating router with updated nlp service usage

--- src/oms_sensemaking/api/routers/nlp.py
+++ src/oms_sensemaking/api/routers/nlp.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from oms_sensemaking.api.schemas.nlp import AnalyzeTextResponse
 from oms_sensemaking.models.nlp import TextSubmission
-from oms_sensemaking.nlp.nlp_service import NlpService
+from oms_sensemaking.nlp.nlp_service import NlpService, NlpStringReader
 
 router: APIRouter = APIRouter()
 
@@ -10,5 +10,6 @@ router: APIRouter = APIRouter()
 @router.post("/ner", response_model=AnalyzeTextResponse, response_model_exclude_none=True, status_code=200)
 def analyze_text(submission: TextSubmission):
     nlp: NlpService = NlpService()
-    nlp.run_nlp_service(submission.text)
+    reader = NlpStringReader(submission.text)
+    nlp.run_nlp(reader)
     return AnalyzeTextResponse(success=True)
