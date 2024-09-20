ce12a555e31369edba82385d7af6bf0ee140e964
charlie.beall <charlie.beall@blackcape.io>
2024-09-20 11:59:47 -0400

    Adding new tests

--- src/oms_sensemaking/nlp/nlp_service.py
+++ src/oms_sensemaking/nlp/nlp_service.py
@@ -14,7 +14,7 @@ class NlpService:
     """Intermediary between the API and the NLP Business Logic"""
 
     # TODO: Adjust input based on what we decide with Core is needed
-    def run_nlp_service(self, text) -> None:
+    def run_nlp_service(self, text) -> bool:
         """
         Pipeline called by API to run the NLP Business Logic and report findings back to OMS
         :param text: The body of text to be analyzed by the NLP Service
@@ -22,7 +22,7 @@ class NlpService:
         submission_data = SubmissionData(document_id=uuid4(), text=text)
         findings = self.run_nlp_sensemaker(submission_data)
         self.submit_findings_to_oms(findings)
-        return None
+        return True
 
     def run_nlp_sensemaker(self, data: SubmissionData) -> EntitiesAndRelationships:
         """
@@ -34,10 +34,10 @@ class NlpService:
         ents_and_rels = nlp_sensemaker.process_data(SubmissionData(document_id=data.document_id, text=data.text))
         return ents_and_rels
 
-    def submit_findings_to_oms(self, findings: EntitiesAndRelationships) -> None:
+    def submit_findings_to_oms(self, findings: EntitiesAndRelationships) -> bool:
         # TODO: Implement this function to call the EntityDecorator
         print(findings)
-        return None
+        return True
 
 
 # TODO: Delete main once the API is up and running. Do the following in the API call
