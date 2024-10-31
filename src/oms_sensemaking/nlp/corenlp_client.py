6767710377395faf3f27aaca4b474b856455ce48
charlie.beall <charlie.beall@blackcape.io>
2024-10-31 12:31:00 -0400

    Logging the corenlp client result as single line

--- src/oms_sensemaking/nlp/corenlp_client.py
+++ src/oms_sensemaking/nlp/corenlp_client.py
@@ -49,7 +49,7 @@ class CoreNlpClient:
         result = httpx.post(self.url, data=data, content=text, timeout=None)
 
         LOGGER.info("CoreNLP Client annotation finished.")
-        LOGGER.debug(f"CoreNLP Client returned result of size {len(result.text)} characters")
+        LOGGER.debug(f"CoreNLP Client returned result {result.text.split("\n")}")
 
         return result.text
 
@@ -76,7 +76,7 @@ class CoreNlpClient:
         result = httpx.post(self.url, data=data, content=text, timeout=None)
 
         LOGGER.info("CoreNLP Client annotation finished.")
-        LOGGER.debug(f"CoreNLP Client returned result of size {len(result.text)} characters")
+        LOGGER.debug(f"CoreNLP Client returned result {result.text.split("\n")}")
 
         # Parsing the xml response to get it as a dictionary for easy indexing later
         formatted_annotations = xmltodict.parse(result.text)["root"]["document"]["sentences"]["sentence"]
