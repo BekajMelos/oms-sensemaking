d53566f3893afe7e3687ff88d80753ae90a2abe4
Jeff Trentacoste <jeff@blackcape.io>
2024-11-14 20:51:54 -0500

    Updated to work across different version of Docker.

--- docker/postgis/initdb.d/30_enable-ssl.sql
+++
@@ -1,5 +0,0 @@
--- 30_enable-ssl.sql --- Enable SSL with custom cert/key.
-ALTER SYSTEM SET ssl_cert_file TO '/opt/common/pki/server.public';
-ALTER SYSTEM SET ssl_key_file TO '/opt/common/pki/server.private';
---ALTER SYSTEM SET ssl_ca_file TO '/opt/common/pki/trusted.crt';
-ALTER SYSTEM SET ssl TO 'ON';
