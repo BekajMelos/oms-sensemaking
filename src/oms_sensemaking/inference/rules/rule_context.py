a4d3dc8419a9d0f87e6f3339cd364955941a7c85
David Nyman <david.nyman@blackcape.io>
2024-11-20 12:52:00 -0500

    Add docstring for RuleContext

--- src/oms_sensemaking/inference/rules/rule_context.py
+++ src/oms_sensemaking/inference/rules/rule_context.py
@@ -5,6 +5,11 @@ from oms_sdk.generated.generated_graphql_client.source import SourceSource
 
 
 class RuleContext:
+    """
+    Map inputs for the rules into well defined models, this gives us direct access
+    to properties in the rules for a variety of object types
+    """
+
     def __init__(self, **kwargs):
         self.attribute: AttributeAttribute = kwargs.get("attribute")
         self.relationship: RelationshipRelationship = kwargs.get("relationship")
