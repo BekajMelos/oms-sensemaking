07f4c5871db5b07878326398e5462e28b9fef253
peterolsen <peterolsen@Peters-MacBook-Pro.local>
2024-11-26 09:17:08 -0500

    Reset node_uuid_list and node_id_mapping to reset each publish

--- src/oms_sensemaking/core/sensemakers.py
+++ src/oms_sensemaking/core/sensemakers.py
@@ -75,6 +75,8 @@ class OmsPublisher(SensemakerPublisher):
     def publish(
         self, data, results
     ) -> tuple[list[CreateNodeCreateNode], list[CreateRelationshipCreateRelationship], list[CreateAttributeCreateAttribute]]:
+        self.node_uuid_list = [] # Make sure old lists doesn't persist on a fresh sensemaker run
+        self.node_id_mapping = {} # Make sure old mappings don't persist on a fresh sensemaker run
         published_nodes = self.publish_nodes(self.format_nodes(data, results))
         published_relationships = self.publish_relationships(self.format_relationships(data, results))
         published_attributes = self.publish_attributes(self.format_attributes(data, results))
