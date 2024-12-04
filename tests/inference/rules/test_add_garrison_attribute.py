2d0dcc7e89535b803d520fdf4803d9c48eade867
Nihar Gudiseva <nihar0825@gmail.com>
2024-12-04 14:28:16 -0500

    Created pytest objects

--- tests/inference/rules/test_add_garrison_attribute.py
+++ tests/inference/rules/test_add_garrison_attribute.py
@@ -15,18 +15,18 @@ from oms_sensemaking.inference.rules.add_has_name_attribute import AddHasNameAtt
 from oms_sensemaking.inference.rules.add_garrison_attribute import AddGarrisonAttribute
 from oms_sensemaking.inference.rules.rule_context import RuleContext
 
-
+# Sample Attributes
 @pytest.fixture
-def garrison_attr(mocker: MockerFixture):
+def initial_attr(mocker: MockerFixture):
     """
     A Garrison attribute to be evaluated
     """
     attr = mocker.Mock(spec=AttributeAttribute)
-    attr.id = "junk won't match"
+    attr.id = "uuid1"
     attr.acm = DEFAULT_ACM
     attr.attributeIri = SETTINGS.inference_add_garrison_attribute_iri
     attr.attributeName = SETTINGS.inference_add_garrison_attribute_iri.split("/")[-1]
-    attr.attributeValue = "some attr name"
+    attr.attributeValue = "Initial Attribute"
     attr.attributeType = AttributeType.BOOLEAN
     attr.confidence = Confidence.MODERATE
     attr.sourceId = "559cf331-ac45-4a78-816a-b4b3835d3dbd"
@@ -40,7 +40,7 @@ def garrison_attr(mocker: MockerFixture):
                 "coordinates": [
                     -2.86242139203938,
                     18.68484166661493
-            ],
+                ],
             "type": "Point"
         }}]
     }
@@ -48,7 +48,68 @@ def garrison_attr(mocker: MockerFixture):
     return attr
 
 @pytest.fixture
-def garrison_node(mocker: MockerFixture):
+def final_attr(mocker: MockerFixture):
+    """
+    A Garrison attribute to be evaluated
+    """
+    attr = mocker.Mock(spec=AttributeAttribute)
+    attr.id = "uuid2"
+    attr.acm = DEFAULT_ACM
+    attr.attributeIri = SETTINGS.inference_add_garrison_attribute_iri
+    attr.attributeName = SETTINGS.inference_add_garrison_attribute_iri.split("/")[-1]
+    attr.attributeValue = "some attr name"
+    attr.attributeType = AttributeType.BOOLEAN
+    attr.confidence = Confidence.MODERATE
+    attr.sourceId = "559cf331-ac45-4a78-816a-b4b3835d3dbd"
+    attr.nodeId = "dd7763a6-dad4-46d7-acfa-d23a648eb143"
+    attr.geo = {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -5.632561898769694,
+                    23.307540107821012
+                ],
+            "type": "Point"
+        }}]
+    }
+
+    return attr
+
+# Sample Nodes
+@pytest.fixture
+def base_node(mocker: MockerFixture):
+    """
+    A Base node to be evaluated
+    """
+    node = mocker.Mock(spec=NodeNode)
+    node.id = "dd7763a6-dad4-46d7-acfa-d23a648eb143"
+    node.name = "TEST"
+    node.geoQuery = "some query"
+    node.relationships = "another query"
+    node.tier = "PRIMARY" #idk what this should be
+    node.attributes = [final_attr]
+
+    return node
+
+@pytest.fixture
+def primary_node(mocker: MockerFixture):
+    """
+    A Garrison attribute to be evaluated
+    """
+    node = mocker.Mock(spec=NodeNode)
+    node.id = "68e2f92d-125f-42ca-8197-27beda61542f"
+    node.name = "TEST"
+    node.geoQuery = "some query"
+    node.relationships = [base_node]
+    node.tier = "PRIMARY"
+
+    return node
+
+@pytest.fixture
+def observational_node(mocker: MockerFixture):
     """
     A Garrison attribute to be evaluated
     """
@@ -56,7 +117,7 @@ def garrison_node(mocker: MockerFixture):
     node.id = "0cc17447-b1f8-48e8-ae30-f9031f250b5d"
     node.name = "TEST"
     node.geoQuery = "some query"
-    node.relationships = "another query"
+    node.relationships = [primary_node]
     node.tier = "OBSERVATIONAL"
 
     return node
@@ -68,27 +129,30 @@ def test_evaluate_none_input():
     rule = AddGarrisonAttribute("some name")
     assert not rule.evaluate(RuleContext()), "should only run for attributes"
 
-def test_action_creates_attribute(mocker: MockerFixture, garrison_attr, garrison_node):
+def test_action_creates_attribute(mocker: MockerFixture, initial_attr, garrison_node):
     print("*********************START ACTION TEST************************")
     mock = mocker.patch("oms_sensemaking.clients.oms_client.create_attribute")
-    mock2 = mocker.patch("oms_sensemaking.clients.oms_client.create_node")
-    mock3 = mocker.patch("oms_sensemaking.clients.oms_client.get_nodes")
-    mock2.return_value = garrison_node
-    mock3.return_value = garrison_node
+    mock_create_node = mocker.patch("oms_sensemaking.clients.oms_client.create_node")
+    mock_get_node = mocker.patch("oms_sensemaking.clients.oms_client.get_nodes")
+
+    mock_create_node.return_value = garrison_node
+    mock_get_node.return_value = garrison_node
+
+
     print("test 1")
     rule = AddGarrisonAttribute("some name")
     print("test 2")
-    rule.action(RuleContext(attribute=garrison_attr))
+    rule.action(RuleContext(attribute=initial_attr))
     print("test 3")
     mock.assert_called_once_with(
         CreateAttributeInput(
             attributeIri=SETTINGS.inference_add_has_name_attribute_meta_data_iri,
             attributeValue="true",
             attributeType=AttributeType.BOOLEAN,
-            confidence=garrison_attr.confidence,
-            sourceId=garrison_attr.sourceId,
-            nodeId=garrison_attr.nodeId,
-            acm=garrison_attr.acm,
+            confidence=initial_attr.confidence,
+            sourceId=initial_attr.sourceId,
+            nodeId=initial_attr.nodeId,
+            acm=initial_attr.acm,
             tags=SETTINGS.inference_tags,
         )
     )
