5e3e65fb9772117332196d3f8a2d70b682a6d308
peterolsen <peterolsen@peters-mbp.gerbil-cloud.ts.net>
2024-12-12 14:30:44 -0500

    Change default objectType in ObjectEvent to OBSERVATION

--- src/oms_sensemaking/core/events.py
+++ src/oms_sensemaking/core/events.py
@@ -50,7 +50,7 @@ class ObjectEvent:
         return ObjectEvent(
             str(data.get("userDn")),
             UUID(data.get("objectId", "")),
-            ObjectType(data.get("objectType", "ATTRIBUTE")),
+            ObjectType(data.get("objectType", "OBSERVATION")),
             Action(data.get("eventType", ""))
         )
 
