795aceef3a9e68af68c2302079973baf9dbfaf66
Thomas Boaz <thomas.boaz@blackcape.io>
2025-02-21 13:50:32 -0500

    Fix similar tracks tests

--- tests_int/geospatial/test_similar_tracks.py
+++ tests_int/geospatial/test_similar_tracks.py
@@ -1,12 +1,14 @@
 """Tests for similar tracks sensemaker."""
 
+from collections.abc import Generator
 from datetime import datetime
-from typing import Iterator
+from typing import Any
 from uuid import uuid4
 
 import pytest
 import shapely
 from oms_sdk import DEFAULT_ACM
+from oms_sdk.generated.generated_graphql_client import Confidence
 from sqlalchemy.orm import Session
 
 from oms_sensemaking.geospatial.sensemakers import SimilarTracksSensemaker
@@ -20,60 +22,155 @@ TRACK_UUID1 = uuid4()
 TRACK_UUID2 = uuid4()
 TRACK_UUID3 = uuid4()
 TRACK_UUID4 = uuid4()
-DATA: list = [  # Latitude, Longitude, Altitude (m), Description, Node ID, Attr ID, detection_time,
+DATA = {  # Latitude, Longitude, Altitude (m), Description, Node ID, Obs ID, detection_time, Obs confidence
     # Track 1
-    [51.482286, -0.165222, None, "London", NODE_UUID1, uuid4(), datetime.fromisoformat("2024-03-20T12:00:00-04:00"),
-    TRACK_UUID1],
-    [51.466103, -0.210562, None, "London", NODE_UUID1, uuid4(), datetime.fromisoformat("2024-03-20T12:10:00-04:00"),
-    TRACK_UUID1],
-    [51.487613, -0.229466, None, "London", NODE_UUID1, uuid4(), datetime.fromisoformat("2024-03-20T12:20:00-04:00"),
-    TRACK_UUID1],
-    # # Track 2
-    [41.467810, 2.289575, None, "Barcelona", NODE_UUID2, uuid4(), datetime.fromisoformat("2024-08-20T16:00:00-04:00"),
-    TRACK_UUID2],
-    [41.399953, 2.217167, None, "Barcelona", NODE_UUID2, uuid4(), datetime.fromisoformat("2024-08-20T16:10:00-04:00"),
-    TRACK_UUID2],
-    [41.356069, 2.183748, None, "Barcelona", NODE_UUID2, uuid4(), datetime.fromisoformat("2024-08-20T16:20:00-04:00"),
-    TRACK_UUID2],
-    [41.296465, 2.130487, None, "Barcelona", NODE_UUID2, uuid4(), datetime.fromisoformat("2024-08-20T16:30:00-04:00"),
-    TRACK_UUID2],
-    # # Track 3
-    [41.467811, 2.289576, None, "Barcelona", NODE_UUID3, uuid4(), datetime.fromisoformat("2024-08-20T16:19:00-04:00"),
-    TRACK_UUID3],
-    [41.399954, 2.217168, None, "Barcelona", NODE_UUID3, uuid4(), datetime.fromisoformat("2024-08-20T16:29:00-04:00"),
-    TRACK_UUID3],
-    [41.356070, 2.183749, None, "Barcelona", NODE_UUID3, uuid4(), datetime.fromisoformat("2024-08-20T16:39:00-04:00"),
-    TRACK_UUID3],
-]
+    TRACK_UUID1: [
+        [
+            51.482286,
+            -0.165222,
+            None,
+            "London",
+            NODE_UUID1,
+            uuid4(),
+            datetime.fromisoformat("2024-03-20T12:00:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            51.466103,
+            -0.210562,
+            None,
+            "London",
+            NODE_UUID1,
+            uuid4(),
+            datetime.fromisoformat("2024-03-20T12:10:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            51.487613,
+            -0.229466,
+            None,
+            "London",
+            NODE_UUID1,
+            uuid4(),
+            datetime.fromisoformat("2024-03-20T12:20:00-04:00"),
+            Confidence.HIGH,
+        ],
+    ],
+    # Track 2
+    TRACK_UUID2: [
+        [
+            41.467810,
+            2.289575,
+            None,
+            "Barcelona",
+            NODE_UUID2,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:00:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            41.399953,
+            2.217167,
+            None,
+            "Barcelona",
+            NODE_UUID2,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:10:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            41.356069,
+            2.183748,
+            None,
+            "Barcelona",
+            NODE_UUID2,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:20:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            41.296465,
+            2.130487,
+            None,
+            "Barcelona",
+            NODE_UUID2,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:30:00-04:00"),
+            Confidence.HIGH,
+        ],
+    ],
+    # Track 3
+    TRACK_UUID3: [
+        [
+            41.467811,
+            2.289576,
+            None,
+            "Barcelona",
+            NODE_UUID3,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:19:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            41.399954,
+            2.217168,
+            None,
+            "Barcelona",
+            NODE_UUID3,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:29:00-04:00"),
+            Confidence.HIGH,
+        ],
+        [
+            41.356070,
+            2.183749,
+            None,
+            "Barcelona",
+            NODE_UUID3,
+            uuid4(),
+            datetime.fromisoformat("2024-08-20T16:39:00-04:00"),
+            Confidence.HIGH,
+        ],
+    ],
+}
 
 
 @pytest.fixture
-def tester_db(db: Session) -> Iterator[Session]:
-    for row in DATA:
-        point: Point = Point(
-            node_id=row[4],
-            node_version=1,
-            observation_id=row[5],
-            observation_version=1,
-            location=f"POINT({row[1]} {row[0]})",  # lng lat
-            altitude=row[2],
-            detection_time=row[6],
-            acm=DEFAULT_ACM,
-            source_id=SOURCE_ID,
-            track_id=row[7]
+def tester_db(db: Session) -> Generator[Session, Any, None]:
+    for track_uuid, rows in DATA.items():
+        points: list[Point] = []
+        for row in rows:
+            point, _ = Point.get_or_create(
+                session=db,
+                node_id=row[4],
+                node_version=1,
+                observation_id=row[5],
+                observation_version=1,
+                location=f"POINT({row[1]} {row[0]})",  # lng lat
+                altitude=row[2],
+                detection_time=row[6],
+                acm=DEFAULT_ACM,
+                source_id=SOURCE_ID,
+                observation_confidence=row[7],
+            )
+            points.append(point)
+        Track.get_or_create(
+            session=db,
+            defaults=dict(
+                points=points,
+                node_id=points[0].node_id,
+                algorithm="similar_tracks_test_track",
+            ),
+            track_uuid=track_uuid,
         )
-
-        db.add(point)
-
-    db.commit()
 
     yield db
 
 
-def test_most_similar_tracks_success_exact_same_path(tester_db):
+def test_most_similar_tracks_success_exact_same_path(tester_db: Session):
     """Test similar track with same path."""
     node_id = uuid4()
-    track_id = uuid4()
+    track_uuid = uuid4()
     # first point is wayyy east of london
     p1 = Point(
         acm=DEFAULT_ACM,
@@ -85,7 +182,7 @@ def test_most_similar_tracks_success_exact_same_path(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     p2 = Point(
@@ -98,7 +195,7 @@ def test_most_similar_tracks_success_exact_same_path(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     p3 = Point(
@@ -111,11 +208,16 @@ def test_most_similar_tracks_success_exact_same_path(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     # Create Track Object
-    track = Track(points=[p1, p2, p3], node_id=uuid4())
+    track = Track(
+        points=[p1, p2, p3],
+        node_id=uuid4(),
+        track_uuid=track_uuid,
+        algorithm="test_track",
+    )
 
     similar_tracks = SimilarTracksSensemaker().execute(track)
 
@@ -126,10 +228,10 @@ def test_most_similar_tracks_success_exact_same_path(tester_db):
     assert similar_tracks.top_similarities.queue[1][1] == TRACK_UUID3
 
 
-def test_most_similar_tracks_success_start(tester_db):
+def test_most_similar_tracks_success_start(tester_db: Session):
     """Test similar track where ending point doesn't match the similar track."""
     node_id = uuid4()
-    track_id = uuid4()
+    track_uuid = uuid4()
 
     p1 = Point(
         acm=DEFAULT_ACM,
@@ -141,7 +243,7 @@ def test_most_similar_tracks_success_start(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     p2 = Point(
@@ -154,7 +256,7 @@ def test_most_similar_tracks_success_start(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     # last point is wayyy west of london
@@ -168,11 +270,16 @@ def test_most_similar_tracks_success_start(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     # Create Track Object
-    track = Track(points=[p1, p2, p3], node_id=uuid4())
+    track = Track(
+        points=[p1, p2, p3],
+        node_id=uuid4(),
+        track_uuid=track_uuid,
+        algorithm="test_track",
+    )
 
     similar_tracks = SimilarTracksSensemaker().execute(track)
 
@@ -180,10 +287,10 @@ def test_most_similar_tracks_success_start(tester_db):
     assert similar_tracks.top_similarities.queue[0][0] == 0.75
 
 
-def test_most_similar_tracks_success_end(tester_db):
+def test_most_similar_tracks_success_end(tester_db: Session):
     """Test similar track where starting point doesn't match the similar track."""
     node_id = uuid4()
-    track_id = uuid4()
+    track_uuid = uuid4()
     # first point is wayyy east of london
     p1 = Point(
         acm=DEFAULT_ACM,
@@ -195,7 +302,7 @@ def test_most_similar_tracks_success_end(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     p2 = Point(
@@ -208,7 +315,7 @@ def test_most_similar_tracks_success_end(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     p3 = Point(
@@ -221,11 +328,16 @@ def test_most_similar_tracks_success_end(tester_db):
         observation_id=uuid4(),
         observation_version=1,
         source_id=uuid4(),
-        track_id=track_id
+        observation_confidence=Confidence.HIGH,
     )
 
     # Create Track Object
-    track = Track(points=[p1, p2, p3], node_id=uuid4())
+    track = Track(
+        points=[p1, p2, p3],
+        node_id=uuid4(),
+        track_uuid=track_uuid,
+        algorithm="test_track",
+    )
 
     similar_tracks = SimilarTracksSensemaker().execute(track)
 
