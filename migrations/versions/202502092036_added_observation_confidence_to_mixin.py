815570bb6c9131c34946e4e1578d1405a1260e85
Thomas Boaz <thomas.boaz@blackcape.io>
2025-02-26 13:19:35 -0500

    Fix migration order

--- migrations/versions/202502092036_added_observation_confidence_to_mixin.py
+++ migrations/versions/202502092036_added_observation_confidence_to_mixin.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "202502092036"
-down_revision: str | None = "202501101242"
+down_revision: str | None = "202501271106"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
