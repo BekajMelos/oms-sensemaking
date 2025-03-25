0aee8391f647d8feb83bd2edec6fe5980c40f205
Aaron Ko <aaron.ko@blackcape.io>
2025-03-25 16:11:02 -0400

    added rule fields in config, altered inference class

--- src/oms_sensemaking/inference/sensemakers/inference.py
+++ src/oms_sensemaking/inference/sensemakers/inference.py
@@ -1,8 +1,10 @@
 import logging
 from typing import Iterable
 
+from oms_sensemaking.config import SETTINGS
 from oms_sensemaking.core.sensemakers import FindingBase, Sensemaker
 from oms_sensemaking.inference.engine.engine import Engine
+from oms_sensemaking.inference.rules.add_garrison_attribute import AddOutOfGarrisonAttribute
 from oms_sensemaking.inference.rules.add_has_name_attribute import AddHasNameAttribute
 from oms_sensemaking.inference.rules.incursions import Incursion
 from oms_sensemaking.inference.rules.rule_context import RuleContext
@@ -15,10 +17,18 @@ class InferenceSensemaker(Sensemaker):
         """Create a new instance of InferenceSensemaker."""
         super().__init__()
         self.version = (1, 0, 0)
-        self.config = {"rules": [AddHasNameAttribute("AddHasNameAttribute"),
-                                 Incursion("Incursion")]}
+        self.config = {
+            "rules": []
+        }
         self.engine = Engine()
-
+        rule_mappings = {
+            "toggle_add_garrison_rule": AddOutOfGarrisonAttribute("AddOutOfGarrisonAttribute"),
+            "toggle_add_has_name_rule": AddHasNameAttribute("AddHasNameAttribute"),
+            "toggle_incursion_rule": Incursion("Incursion"),
+        }
+        for setting_name, rule in rule_mappings.items():
+            if getattr(SETTINGS, setting_name, False):  # Check if toggle is True
+                self.config["rules"].append(rule)
         for rule in self.config.get("rules", []):
             self.engine.add_rule(rule)
 
