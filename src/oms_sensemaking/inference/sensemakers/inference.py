6893427ace82c780f47d7d7a99948469cc2a03e7
Alisha Grover <alisha.grover@blackcape.io>
2025-03-26 00:11:55 -0400

    lint

--- src/oms_sensemaking/inference/sensemakers/inference.py
+++ src/oms_sensemaking/inference/sensemakers/inference.py
@@ -3,8 +3,8 @@ from typing import Iterable
 
 from oms_sensemaking.core.sensemakers import FindingBase, Sensemaker
 from oms_sensemaking.inference.engine.engine import Engine
-from oms_sensemaking.inference.rules.in_out_garrison import InOrOutOfGarrison
 from oms_sensemaking.inference.rules.add_has_name_attribute import AddHasNameAttribute
+from oms_sensemaking.inference.rules.in_out_garrison import InOrOutOfGarrison
 from oms_sensemaking.inference.rules.incursions import Incursion
 from oms_sensemaking.inference.rules.rule_context import RuleContext
 
