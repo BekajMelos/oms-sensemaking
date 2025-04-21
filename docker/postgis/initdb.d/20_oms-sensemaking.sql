138881daf95034a8beea30199d082ebf2eafc465
Aaron Ko <aaron.ko@blackcape.io>
2025-04-21 16:18:57 -0400

    changed line in db init file for testing, added encoding logic to config, iterpolation taken care of in env.py

--- docker/postgis/initdb.d/20_oms-sensemaking.sql
+++ docker/postgis/initdb.d/20_oms-sensemaking.sql
@@ -1,5 +1,5 @@
 -- 20_db-user.sql --- Create databases and database user for OMS Sensemaking.
-CREATE USER appuser WITH ENCRYPTED PASSWORD 'password';
+CREATE USER appuser WITH ENCRYPTED PASSWORD 'pas:swo@rd#';
 
 --------------------
 -- App Database
