7ae347666a70fb4799ee7084a1380b1014c6872d
Aaron Ko <aaron.ko@blackcape.io>
2025-04-30 11:57:16 -0400

    added ability to view different formats of rdf, prettier 'print'

--- src/oms_sensemaking/api/routers/rdf.py
+++ src/oms_sensemaking/api/routers/rdf.py
@@ -12,8 +12,9 @@ LOGGER: logging.Logger = logging.getLogger(__name__)
 rdf_client = RDFClient()
 
 @router.get('/resolver/{obj_id}')
-def rdf_resolver(obj_id: UUID4):
-    rdfs = rdf_client.get_rdf_from_id(obj_id, oms_crud_tool)
+@router.get('/resolver/{obj_id}.{format}')
+def rdf_resolver(obj_id: UUID4, format: str = "turtle"):
+    rdfs = rdf_client.get_rdf_from_id(obj_id, format, oms_crud_tool)
     if not rdfs:
         LOGGER.error("Invalid Object. Ensure the Id is correct")
         raise HTTPException(status_code=404, detail="Object not found")
--- src/oms_sensemaking/clients/rdf_client.py
+++ src/oms_sensemaking/clients/rdf_client.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from fastapi import HTTPException, Response
 from pydantic import UUID4
 from rdflib import Graph, Literal, URIRef
 
@@ -9,13 +10,13 @@ from oms_sensemaking.core.oms_crud import OmsCrudTool
 LOGGER: logging.Logger = logging.getLogger(__name__)
 
 class RDFClient:
-    def get_rdf_from_id(self, obj_id: UUID4, oms_crud_tool: OmsCrudTool):
+    def get_rdf_from_id(self, obj_id: UUID4, format: str, oms_crud_tool: OmsCrudTool):
         """Return service information."""
         LOGGER.info(f"RDF API request from object: {obj_id}")
         try:
             node = oms_crud_tool.get_node(obj_id)
             obj_json = node.model_dump_json()
-            return self.json_to_rdf(obj_json)
+            return self.json_to_rdf(obj_json, format)
         except KeyError as e:
             LOGGER.error(f"KeyError: Missing expected key in the object JSON - {str(e)}")
         except TypeError as e:
@@ -28,7 +29,9 @@ class RDFClient:
             LOGGER.error(f"Unexpected error: {repr(e)}")
         return None
 
-    def json_to_rdf(self, obj):
+    def json_to_rdf(self, obj, format):
+        if format not in ["turtle", "json-ld", "n3", "nt"]:
+            raise HTTPException(status_code=400, detail="Unsupported format")
         dict_obj = json.loads(obj)
         g = Graph()
 
@@ -51,5 +54,5 @@ class RDFClient:
         for key, value in dict_obj.items():
             if key != "id" and key != "permissions":
                 add_triples(subject, value, prefix=key + "_")
-
-        return g.serialize(format="turtle")
+        output = g.serialize(format=format, sort=True)
+        return Response(content=output, media_type="text/plain")
