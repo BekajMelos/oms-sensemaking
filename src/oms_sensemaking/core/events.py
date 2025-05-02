284a90e424f23f2f12d9074434dbf55c3fb51277
Mary Shultz <maryshultz@Mac.lan>
2025-05-02 12:55:01 -0400

    Update for unified file and inc-19

--- src/oms_sensemaking/core/events.py
+++ src/oms_sensemaking/core/events.py
@@ -151,7 +151,7 @@ class BaseRabbitMQListener(AuditLogEventConsumer):
             )
             self._connection = BlockingConnection(parameters)
             self._channel = self._connection.channel()
-            self._channel.queue_declare(queue=self._queue_name, durable=True)
+            self._channel.queue_declare(queue=self._queue_name, durable=True, arguments={"x-delivery-limit": -1, "x-queue-type": "quorum"})
             LOGGER.info(f"Connected to RabbitMQ queue: {self._queue_name}")
             return True
         except (AMQPConnectionError, AMQPChannelError) as ex:
