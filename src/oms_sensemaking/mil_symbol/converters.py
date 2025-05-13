9d590f65e0792ad7de237c55602ee3cb70166842
Aaron Ko <aaron.ko@blackcape.io>
2025-05-13 13:30:53 -0400

    Added new base class for B and C standard, separated affiliation for B and C

--- src/oms_sensemaking/mil_symbol/converters.py
+++ src/oms_sensemaking/mil_symbol/converters.py
@@ -13,23 +13,23 @@ def to_2525c_from_2525b(code_2525b: MilSymbol2525B, settings: Dict) -> MilSymbol
         code_2525c = MilSymbol2525C(SETTINGS.mil_symbol_settings.default_2525c_code, settings)
 
         # convert standard identity
-        standard_identity = code_2525b.code[MilSymbol2525B.MIL_SYM_2525B_STD_IDENTITY_IDX]
+        standard_identity = code_2525b.code[MilSymbol2525B.MIL_SYM_2525_B_C_STD_IDENTITY_IDX]
         standard_identity_list = settings["MIL_SYMBOL_2525B"]["STANDARD_IDENTITY_LISTS"][standard_identity]
         for code, other_standard_identity_list in settings["MIL_SYMBOL_2525C"]["STANDARD_IDENTITY_LISTS"].items():
             # if the lists have values in common then that's the equivalent code
             if set(standard_identity_list) & set(other_standard_identity_list):
-                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_STD_IDENTITY_IDX, code)
+                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_STD_IDENTITY_IDX, code)
                 LOGGER.debug(f'Equivalent std identity is : {code} b/c {other_standard_identity_list}')
                 break
 
         # convert dimension
-        dimension = code_2525b.code[MilSymbol2525B.MIL_SYM_2525B_DIMENSION_IDX]
-        code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_DIMENSION_IDX, dimension)
+        dimension = code_2525b.code[MilSymbol2525B.MIL_SYM_2525_B_C_DIMENSION_IDX]
+        code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_DIMENSION_IDX, dimension)
         LOGGER.debug(f'Equivalent dimension is : {dimension}, 2525b & 2525c share dimension')
 
         # convert status
-        status = code_2525b.code[MilSymbol2525B.MIL_SYM_2525B_STATUS_IDX]
-        code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_STATUS_IDX, status)
+        status = code_2525b.code[MilSymbol2525B.MIL_SYM_2525_B_C_STATUS_IDX]
+        code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_STATUS_IDX, status)
         LOGGER.debug(f'Equivalent status is : {code}, 2525b & 2525c share status')
 
         return code_2525c
@@ -45,7 +45,7 @@ def to_2525c(code_2525d: MilSymbol2525D, settings: Dict) -> MilSymbol2525C:
         for code, other_standard_identity_list in settings["MIL_SYMBOL_2525C"]["STANDARD_IDENTITY_LISTS"].items():
             # if the lists have values in common then that's the equivalent code
             if set(standard_identity_list) & set(other_standard_identity_list):
-                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_STD_IDENTITY_IDX, code)
+                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_STD_IDENTITY_IDX, code)
                 LOGGER.debug(f'Equivalent std identity is : {code} b/c {other_standard_identity_list}')
                 break
 
@@ -53,20 +53,20 @@ def to_2525c(code_2525d: MilSymbol2525D, settings: Dict) -> MilSymbol2525C:
         dimension = (code_2525d.code[MilSymbol2525D.MIL_SYM_2525D_DIMENSION_IDX_0] +
                      code_2525d.code[MilSymbol2525D.MIL_SYM_2525D_DIMENSION_IDX_1])
         dimension_list = settings["MIL_SYMBOL_2525D"]["DIMENSION_IRIS"][dimension]
-        for code, other_dimension_list in settings["MIL_SYMBOL_2525C"]["DIMENSION_IRIS"].items():
+        for code, other_dimension_list in settings["MIL_SYMBOL_2525_B_C"]["DIMENSION_IRIS"].items():
             # if the lists have values in common then that's the equivalent code
             if set(dimension_list) & set(other_dimension_list):
-                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_DIMENSION_IDX, code)
+                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_DIMENSION_IDX, code)
                 LOGGER.debug(f'Equivalent dimension is : {code} b/c {other_dimension_list}')
                 break
 
         # convert status
         status = code_2525d.code[MilSymbol2525D.MIL_SYM_2525D_STATUS_IDX]
         status_list = settings["MIL_SYMBOL_2525D"]["STATUS_LISTS"][status]
-        for code, other_status_list in settings["MIL_SYMBOL_2525C"]["STATUS_LISTS"].items():
+        for code, other_status_list in settings["MIL_SYMBOL_2525_B_C"]["STATUS_LISTS"].items():
             # if the lists have values in common then that's the equivalent code
             if set(status_list) & set(other_status_list):
-                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525C_STATUS_IDX, code)
+                code_2525c.update_code(MilSymbol2525C.MIL_SYM_2525_B_C_STATUS_IDX, code)
                 LOGGER.debug(f'Equivalent status is : {code} b/c {other_status_list}')
                 break
 
@@ -77,7 +77,7 @@ def to_2525d(code_2525c: MilSymbol2525C, settings: Dict) -> MilSymbol2525D:
     code_2525d = MilSymbol2525D(SETTINGS.mil_symbol_settings.default_2525d_code, settings)
 
     # convert standard identity
-    standard_identity = code_2525c.code[MilSymbol2525C.MIL_SYM_2525C_STD_IDENTITY_IDX]
+    standard_identity = code_2525c.code[MilSymbol2525C.MIL_SYM_2525_B_C_STD_IDENTITY_IDX]
     standard_identity_list = settings["MIL_SYMBOL_2525C"]["STANDARD_IDENTITY_LISTS"][standard_identity]
     for code, other_standard_identity_list in settings["MIL_SYMBOL_2525D"]["STANDARD_IDENTITY_LISTS"].items():
         # if the lists have values in common then that's the equivalent code
@@ -87,8 +87,8 @@ def to_2525d(code_2525c: MilSymbol2525C, settings: Dict) -> MilSymbol2525D:
             break
 
     # convert dimension
-    dimension = code_2525c.code[MilSymbol2525C.MIL_SYM_2525C_DIMENSION_IDX]
-    dimension_list = settings["MIL_SYMBOL_2525C"]["DIMENSION_IRIS"][dimension]
+    dimension = code_2525c.code[MilSymbol2525C.MIL_SYM_2525_B_C_DIMENSION_IDX]
+    dimension_list = settings["MIL_SYMBOL_2525_B_C"]["DIMENSION_IRIS"][dimension]
     for code, other_dimension_list in settings["MIL_SYMBOL_2525D"]["DIMENSION_IRIS"].items():
         # if the lists have values in common then that's the equivalent code
         if set(dimension_list) & set(other_dimension_list):
@@ -98,8 +98,8 @@ def to_2525d(code_2525c: MilSymbol2525C, settings: Dict) -> MilSymbol2525D:
             break
 
     # convert status
-    status = code_2525c.code[MilSymbol2525C.MIL_SYM_2525C_STATUS_IDX]
-    status_list = settings["MIL_SYMBOL_2525C"]["STATUS_LISTS"][status]
+    status = code_2525c.code[MilSymbol2525C.MIL_SYM_2525_B_C_STATUS_IDX]
+    status_list = settings["MIL_SYMBOL_2525_B_C"]["STATUS_LISTS"][status]
     for code, other_status_list in settings["MIL_SYMBOL_2525D"]["STATUS_LISTS"].items():
         # if the lists have values in common then that's the equivalent code
         if set(status_list) & set(other_status_list):
---
+++ src/oms_sensemaking/mil_symbol/std_2525_b_c.py
@@ -0,0 +1,120 @@
+"""Class For Shared Methods and Properties between 2525C and 2525B"""
+
+import logging
+from typing import List, Optional
+
+from oms_sdk.generated.generated_graphql_client import AttributeAttribute, NodeNode
+
+from oms_sensemaking.mil_symbol.mil_symbol_std import MilSymbol
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MilSymbol2525BandC(MilSymbol):
+    """
+    Parent class for 2525B and 2525C.
+
+    2525B and 2525C inherit all properties of this class
+    and overwrite affiliation.
+    """
+
+    UNKNOWN_DIMENSION_CODE = "Z"
+
+    # E.g.  SUZP------*****
+    MIL_SYM_2525_B_C_STD_IDENTITY_IDX = 1
+    MIL_SYM_2525_B_C_DIMENSION_IDX = 2
+    MIL_SYM_2525_B_C_STATUS_IDX = 3
+
+
+    @property
+    def formatted_code(self) -> str:
+        "Return the formatted code"
+        return self.code
+
+    def enrich(self,
+               affiliation_attr: Optional[AttributeAttribute],
+               oms_node: NodeNode,
+               ancestor_iris: List[str],
+               status_attr: Optional[AttributeAttribute]) -> None:
+        """Enrich the code given node attribute data
+
+        :param affiliation_attr: Optional Attribute for the affiliation
+        :param oms_node: Node we are processing
+        :param ancestor_iris: Node's ancestor iri list
+        :param status_attr: Optional Attribute for the status
+        """
+
+        self.enrich_affiliation(affiliation_attr)
+        self.enrich_dimension(oms_node, ancestor_iris)
+        self.enrich_status(status_attr)
+
+    def enrich_affiliation(self, affiliation_attr: Optional[AttributeAttribute]) -> None:
+        """Update Affilation
+
+        :param affiliation_attr: Attribute for the affiliation
+        :return: None
+        """
+
+        if affiliation_attr:
+            node_standard_identity = affiliation_attr.attributeValue
+            # Default to 2525C config settings for affiliation, gets overwritten in child classes
+            for code, standard_identity_list in self.settings["MIL_SYMBOL_2525C"]["STANDARD_IDENTITY_LISTS"].items():
+                if node_standard_identity.lower() in standard_identity_list:
+                    self.update_code(self.MIL_SYM_2525_B_C_STD_IDENTITY_IDX, code)
+                    self.source_ids.put((1, affiliation_attr.sourceId))
+                    self.acms.append(affiliation_attr.acm)
+                    LOGGER.debug(f'Updated std identity: {code} b/c {node_standard_identity}')
+                    break
+
+    def enrich_dimension(self, oms_node: NodeNode, ancestor_iris: List[str]) -> None:
+        """Update Dimension
+
+        Use node's IRIs to update list. If not found and is Unknown, use ancestor IRIs
+
+        :param iri: Node's class iri
+        :return: None
+        """
+
+        def update_dimension(current_iri: str) -> bool:
+            """if iri matches dimension rules, update code
+
+            :param current_iri: Iri to check
+            :return: boolean indicating whether update was made or not
+            """
+            current_iri = current_iri.lower()
+            for code, dimension_iris in self.settings["MIL_SYMBOL_2525_B_C"]["DIMENSION_IRIS"].items():
+                dimension_iris = [dimension_iri.lower() for dimension_iri in dimension_iris]
+                if current_iri in dimension_iris:
+                    self.update_code(self.MIL_SYM_2525_B_C_DIMENSION_IDX, code)
+                    self.acms.append(oms_node.acm)
+                    LOGGER.debug(f'Updated dimension: {code} b/c {current_iri}')
+                    return True
+            return False
+
+        if update_dimension(oms_node.classIri):
+            return
+
+        dimension_code = self.code[self.MIL_SYM_2525_B_C_DIMENSION_IDX]
+        code_is_unknown = dimension_code == self.UNKNOWN_DIMENSION_CODE
+        if code_is_unknown:
+            LOGGER.debug(f'Dimension code is still unknown. Checking ancestor iris: {ancestor_iris}')
+            for iri in ancestor_iris:
+                if update_dimension(iri):
+                    return
+
+    def enrich_status(self, status_attr: Optional[AttributeAttribute]) -> None:
+        """Update Status
+
+        :param status_attr: Attribute for the status
+        :return: None
+        """
+
+        if status_attr:
+            status = status_attr.attributeValue
+            for code, status_list in self.settings["MIL_SYMBOL_2525_B_C"]["STATUS_LISTS"].items():
+                if status.lower() in status_list:
+                    self.update_code(self.MIL_SYM_2525_B_C_STATUS_IDX, code)
+                    self.source_ids.put((2, status_attr.sourceId))
+                    self.acms.append(status_attr.acm)
+                    LOGGER.debug(f'Updated status: {code} b/c {status}')
+                    break
