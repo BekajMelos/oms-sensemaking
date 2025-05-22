66d4c2b714e582cb06b2b4ec742681b7304cb9c9
DeMario Hamilton <demario@blackcape.io>
2025-05-22 17:52:14 -0400

    Handling connection errors to RabbitMQ

---
+++ tests/core/test_events.py
@@ -0,0 +1,27 @@
+"""Module for testing event listeners"""
+import socket
+from unittest import mock
+
+from oms_sensemaking.core.events import BaseRabbitMQListener
+
+
+class DummyRabbitMQListener(BaseRabbitMQListener):
+    """Dummy RabbitMQ Listener class"""
+
+    def process_audit_log_events(self) -> None:
+        """
+        Process audit log events from a RabbitMQ queue.
+
+        Subclasses should override this and call ``self.event_handler.handle_event`` for
+        each ``AuditLogEvent`` processed.
+        """
+        print("Fake processing data. Calling self._connect()")
+        return self._connect()
+
+
+@mock.patch("oms_sensemaking.core.events.BlockingConnection")
+def test_rmq_listeners(mock_connection: mock.MagicMock):
+    """Test exception handling with rabbit mq connections"""
+    mock_connection.side_effect = [socket.gaierror]
+    listener = DummyRabbitMQListener("test", "test-queue", None, None)
+    assert not listener.process_audit_log_events()
