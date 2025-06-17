954fe456fcc689b2bac363a70b68f720321bb60b
Aaron Ko <aaron.ko@blackcape.io>
2025-06-17 15:46:31 -0400

    reworking rate limiting

--- src/oms_sensemaking/core/oms_crud.py
+++ src/oms_sensemaking/core/oms_crud.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from typing import List, Optional, Union
 from uuid import UUID
 
@@ -63,6 +64,29 @@ from ratelimit import limits
 from oms_sensemaking.config import SETTINGS
 
 
+def rate_limit_methods(rate_decorator):
+    def class_decorator(cls):
+        for cls_attribute_name, cls_attribute_value in cls.__dict__.items():
+            if cls_attribute_name.startswith("__"):
+                continue
+            if callable(cls_attribute_value):
+                wrapped = rate_decorator(cls_attribute_value)
+                setattr(cls, cls_attribute_name, wrapped)
+        return cls
+
+    return class_decorator
+
+
+def rate_decorator(function):
+    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        return function(*args, **kwargs)
+
+    return wrapper
+
+
+@rate_limit_methods(rate_decorator)
 class OmsCrudTool:
     """Tool for using OMS_SDK CRUD operations"""
 
@@ -76,7 +100,6 @@ class OmsCrudTool:
             pkcs12_password=SETTINGS.pkcs12_password,
         )
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def publish_nodes(self, nodes: list[CreateNodeInput]) -> list[CreateNodeCreateNode]:
         """
         Publish the Nodes to OMS
@@ -85,7 +108,6 @@ class OmsCrudTool:
         # 1. for each node, publish it to OMS
         return [self.oms_client.create_node(node) for node in nodes]
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def publish_relationships(
         self, relationships: list[CreateRelationshipInput]
     ) -> list[CreateRelationshipCreateRelationship]:
@@ -96,7 +118,6 @@ class OmsCrudTool:
         # 1. for each relationship, publish it to OMS
         return [self.oms_client.create_relationship(relationship) for relationship in relationships]
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def publish_attributes(self, attributes: list[CreateAttributeInput]) -> list[CreateAttributeCreateAttribute]:
         """
         Publish the attributes to oms
@@ -105,7 +126,6 @@ class OmsCrudTool:
         # 1. for each attribute, publish it to OMS
         return [self.oms_client.create_attribute(attribute) for attribute in attributes]
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_node(self, node_input: CreateNodeInput) -> CreateNodeCreateNode:
         """
         Publish the Nodes to OMS
@@ -114,14 +134,12 @@ class OmsCrudTool:
         # 1. for each node, publish it to OMS
         return self.oms_client.create_node(node_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_observation(self, observation_input: CreateObservationInput) -> CreateObservationCreateObservation:
         """
         Publish the Observations to OMS
         """
         return self.oms_client.create_observation(observation_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_relationship(self, relationship_input: CreateRelationshipInput) -> CreateRelationshipCreateRelationship:
         """
         Publish the relationship to OMS
@@ -130,7 +148,6 @@ class OmsCrudTool:
         # 1. for each relationship, publish it to OMS
         return self.oms_client.create_relationship(relationship_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_attribute(self, attribute_input: CreateAttributeInput) -> CreateAttributeCreateAttribute:
         """
         Publish the attribute to oms
@@ -139,7 +156,6 @@ class OmsCrudTool:
         # 1. for each attribute, publish it to OMS
         return self.oms_client.create_attribute(attribute_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_activity(self, activity_input: CreateActivityInput) -> CreateActivityCreateActivity:
         """
         Publish the activity to oms
@@ -148,7 +164,6 @@ class OmsCrudTool:
         # 1. for each activity, publish it to OMS
         return self.oms_client.create_activity(activity_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_source(self, source_input: CreateSourceInput) -> CreateSourceCreateSource:
         """
         Create a source in OMS
@@ -156,7 +171,6 @@ class OmsCrudTool:
         """
         return self.oms_client.create_source(source_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_provider(self, provider_input: CreateProviderInput) -> CreateProviderCreateProvider:
         """
         Create a provider in OMS
@@ -164,7 +178,6 @@ class OmsCrudTool:
         """
         return self.oms_client.create_provider(provider_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_originator(self, originator_input: CreateOriginatorInput) -> CreateOriginatorCreateOriginator:
         """
         Create an Originator in OMS
@@ -173,135 +186,110 @@ class OmsCrudTool:
         return self.oms_client.create_originator(originator_input)
 
     ### GET ###
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_activity(self, id: UUID) -> ActivityActivity:
         """Get existing Activity from OMS"""
         activity = self.oms_client.activity(IdQuery(id=id))
         return activity
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_attribute(self, id: UUID) -> AttributeAttribute:
         """Get existing Attribute from OMS"""
         attribute = self.oms_client.attribute(IdQuery(id=id))
         return attribute
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_node(self, id: UUID) -> NodeNode:
         """Get existing Node from OMS"""
         node = self.oms_client.node(IdQuery(id=id))
         return node
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_observation(self, id: UUID) -> ObservationObservation:
         """Get existing Observation from OMS"""
         observation = self.oms_client.observation(IdQuery(id=id))
         return observation
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_nodes(self, node_info: NodeQuery) -> NodesNodes:
         """Get existing Node from OMS"""
         nodes = self.oms_client.nodes(query=node_info)
         return nodes
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_relationships(self, relationship_info: RelationshipQuery) -> RelationshipsRelationships:
         """Get existing Relationships from OMS"""
         relationships = self.oms_client.relationships(query=relationship_info)
         return relationships
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_attributes(self, attribute_info: AttributeQuery) -> AttributesAttributes:
         """Get existing Attributes from OMS"""
         attributes = self.oms_client.attributes(query=attribute_info)
         return attributes
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_activities(self, activity_info: ActivityQuery) -> ActivitiesActivities:
         """Get existing Activities from OMS"""
         activities = self.oms_client.activities(query=activity_info)
         return activities
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_observations(self, observation_info: ObservationQuery) -> ObservationsObservations:
         """Get existing Observations from OMS"""
         observations = self.oms_client.observations(query=observation_info)
         return observations
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_source(self, source_id: str) -> SourceSource:
         """Get existing Attributes from OMS"""
         source = self.oms_client.source(query=IdQuery(id=source_id))
         return source
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_source_by_name(self, source_name: str) -> SourcesSources:
         return self.oms_client.sources(query=SourceQuery(name=StringQuery(equals=source_name)))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_provider_by_name(self, provider_name: str) -> ProvidersProviders:
         """Get existing Attributes from OMS"""
         return self.oms_client.providers(query=ProviderQuery(name=StringQuery(equals=provider_name)))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_originator_by_name(self, originator_name: str) -> OriginatorsOriginators:
         """Get existing Attributes from OMS"""
         return self.oms_client.originators(query=OriginatorQuery(name=StringQuery(equals=originator_name)))
 
     ### UPDATE ###
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def update_node(self, update_input: UpdateNodeInput) -> UpdateNodeUpdateNode:
         """Update node"""
         return self.oms_client.update_node(update_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def update_relationship(self, update_input: UpdateRelationshipInput) -> UpdateRelationshipUpdateRelationship:
         """Update relationship"""
         return self.oms_client.update_relationship(update_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def update_attribute(self, update_input: UpdateAttributeInput) -> UpdateAttributeUpdateAttribute:
         """Update attribute"""
         return self.oms_client.update_attribute(update_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def update_source(self, update_input: UpdateSourceInput) -> UpdateSourceUpdateSource:
         """Update source"""
         return self.oms_client.update_source(update_input)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def update_activity(self, update_input: UpdateActivityInput) -> UpdateActivityUpdateActivity:
         """Update activity"""
         return self.oms_client.update_activity(update_input)
 
     ### DELETE ###
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_node(self, node_id: str) -> bool:
         """Update node"""
         return self.oms_client.delete_node(DeleteByIdInput(id=node_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_relationship(self, relationship_id: str) -> bool:
         """Update relationship"""
         return self.oms_client.delete_relationship(DeleteByIdInput(id=relationship_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_attribute(self, attribute_id: str) -> bool:
         """Update attribute"""
         return self.oms_client.delete_attribute(DeleteByIdInput(id=attribute_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_provider(self, provider_id: str) -> bool:
         return self.oms_client.delete_provider(DeleteByIdInput(id=provider_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_originator(self, originator_id: str) -> bool:
         return self.oms_client.delete_originator(DeleteByIdInput(id=originator_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def delete_source(self, source_id) -> bool:
         return self.oms_client.delete_source(DeleteByIdInput(id=source_id))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_node_attribute_by_iri(self, node_id: UUID, iris: List[str]) -> List[AttributeAttribute]:
         """
         Given a node id and a list of IRIs, get the attribute values from OMS
@@ -316,7 +304,6 @@ class OmsCrudTool:
             return attributes_response.data
         return []
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def rehydrate_oms_obj(
         self, object_id: UUID, object_type: ObjectType
     ) -> Union[ActivityActivity, AttributeAttribute, ObservationObservation, NodeNode]:
@@ -334,7 +321,6 @@ class OmsCrudTool:
 
         return obj_getter_mapping[object_type](object_id)
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def get_ontology_class(self, iri: str) -> Optional[OntologyClassOntologyClass]:
         """Get the Ontology Class for a given iri
 
@@ -343,7 +329,6 @@ class OmsCrudTool:
         """
         return self.oms_client.ontology_class(query=IriQuery(iri=iri))
 
-    @limits(calls=SETTINGS.maximum_oms_api_calls, period=SETTINGS.oms_api_call_period)
     def create_test_source(
         self,
         test_originator_name: str = "nlp_test_originator",
