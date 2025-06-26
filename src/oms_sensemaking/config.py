32efc878275327d3dcf03ab6a5d012c59abe1054
Aaron Ko <aaron.ko@blackcape.io>
2025-06-26 19:03:40 -0400

    changing time period config

--- src/oms_sensemaking/config.py
+++ src/oms_sensemaking/config.py
@@ -163,7 +163,7 @@ class Settings(BaseSettings):
     # Request Rate Settings
     maximum_oms_api_calls: int = Field(500,
                                        description="Maximum amount of requests made to the OMS API per time period")
-    oms_api_call_period_seconds: int = Field(120,
+    oms_api_call_period_seconds: int = Field(30,
                                              description="Alloted amount of time for maximum OMS API calls to be made")
 
     # Labels
