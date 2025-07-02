8f62b9443d48706f7ab466a50f0f000b9925039b
Aaron Ko <aaron.ko@blackcape.io>
2025-07-02 15:52:21 -0400

    change location of where to cp ssl cert key pair

--- docker/postgis/initdb.d/30_enable-ssl.sh
+++ docker/postgis/initdb.d/30_enable-ssl.sh
@@ -7,21 +7,21 @@ if [ -f /opt/common/pki/server.public ] && [ -f /opt/common/pki/server.private ]
   mkdir -p /var/lib/postgresql
 
   # copy files to writable directory inside the container
-  cp /opt/common/pki/server.public /var/lib/postgresql/server.crt
-  cp /opt/common/pki/server.private /var/lib/postgresql/server.key
+  cp /opt/common/pki/server.public /var/lib/postgresql/data/server.crt
+  cp /opt/common/pki/server.private /var/lib/postgresql/data/server.key
 
   # explicitly set ownership to match the user running postgresql/postgis
-  chown postgres: /var/lib/postgresql/server.crt
-  chown postgres: /var/lib/postgresql/server.key
+  chown postgres: /var/lib/postgresql/data/server.crt
+  chown postgres: /var/lib/postgresql/data/server.key
 
   # set file permissions
-  chmod 600 /var/lib/postgresql/server.key
-  chmod 640 /var/lib/postgresql/server.crt
+  chmod 600 /var/lib/postgresql/data/server.key
+  chmod 640 /var/lib/postgresql/data/server.crt
 
   # configure and enable SSL
   psql << EOF
-ALTER SYSTEM SET ssl_cert_file TO '/var/lib/postgresql/server.crt';
-ALTER SYSTEM SET ssl_key_file TO '/var/lib/postgresql/server.key';
+ALTER SYSTEM SET ssl_cert_file TO '/var/lib/postgresql/data/server.crt';
+ALTER SYSTEM SET ssl_key_file TO '/var/lib/postgresql/data/server.key';
 ALTER SYSTEM SET ssl TO 'ON';
 EOF
 else
