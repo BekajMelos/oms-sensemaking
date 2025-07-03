4e0cb713801f03d8814c520bf738ab02d468c602
Aaron Ko <aaron.ko@blackcape.io>
2025-07-03 10:55:52 -0400

    model mounting after core, made /opt/common/pki in the container read and write no longer read only

--- docker/postgis/initdb.d/30_enable-ssl.sh
+++ docker/postgis/initdb.d/30_enable-ssl.sh
@@ -1,27 +1,20 @@
 # 30_enable-ssl.sql --- Enable SSL with custom keypair.
 
-
 # Copy certs to a writable directory and set ownership/permissions
 if [ -f /opt/common/pki/server.public ] && [ -f /opt/common/pki/server.private ]; then
-  # ensure target dir exists
-  mkdir -p /var/lib/postgresql
-
-  # copy files to writable directory inside the container
-  cp /opt/common/pki/server.public /var/lib/postgresql/data/server.crt
-  cp /opt/common/pki/server.private /var/lib/postgresql/data/server.key
 
   # explicitly set ownership to match the user running postgresql/postgis
-  chown postgres: /var/lib/postgresql/data/server.crt
-  chown postgres: /var/lib/postgresql/data/server.key
+  chown postgres: /opt/common/pki/server.public
+  chown postgres: /opt/common/pki/server.private
 
   # set file permissions
-  chmod 600 /var/lib/postgresql/data/server.key
-  chmod 640 /var/lib/postgresql/data/server.crt
+  chmod 600 /opt/common/pki/server.private
+  chmod 640 /opt/common/pki/server.public
 
   # configure and enable SSL
   psql << EOF
-ALTER SYSTEM SET ssl_cert_file TO '/var/lib/postgresql/data/server.crt';
-ALTER SYSTEM SET ssl_key_file TO '/var/lib/postgresql/data/server.key';
+ALTER SYSTEM SET ssl_cert_file TO '/opt/common/pki/server.public';
+ALTER SYSTEM SET ssl_key_file TO '/opt/common/pki/server.private';
 ALTER SYSTEM SET ssl TO 'ON';
 EOF
 else
