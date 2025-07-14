d7674e97affbb48a2c032682034244f5c7e9d7e7
Aaron Ko <aaron.ko@blackcape.io>
2025-07-14 15:41:54 -0400

    added logic to catch user_dn and make sure it exists/is valid

--- src/oms_sensemaking/api/routers/rdf.py
+++ src/oms_sensemaking/api/routers/rdf.py
@@ -1,10 +1,11 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, Request, Response
 
 from oms_sensemaking.api.schemas.rdf_format import RDFFormat
 from oms_sensemaking.clients.instances import oms_crud_tool
 from oms_sensemaking.clients.rdf_client import RDFClient
+from oms_sensemaking.config import SETTINGS
 
 router: APIRouter = APIRouter()
 
@@ -13,17 +14,23 @@ LOGGER: logging.Logger = logging.getLogger(__name__)
 
 @router.get("/{obj_id:path}")
 @router.get("/{obj_id:path}.{format}")
-def rdf_resolver(obj_id: str, format: RDFFormat = RDFFormat.turtle) -> Response:
+def rdf_resolver(obj_id: str, request: Request, format: RDFFormat = RDFFormat.turtle) -> Response:
     """
     Resolve an object ID into its RDF representation.
 
     Args:
         obj_id (str): The ID of the object to resolve.
+        request (Request): The request received for the RDF data.
         format (RDFFormat): RDF serialization format. Defaults to 'turtle'.
 
     Returns:
         Response: A FastAPI Response object containing the serialized RDF data.
     """
+    request_user_dn = request.headers.get("user_dn")
+    if not request_user_dn:
+        raise HTTPException(status_code=401, detail="Missing user_dn.")
+    if request_user_dn != SETTINGS.user_dn:
+        raise HTTPException(status_code=401, detail="Incorrect credentials: user_dn")
     rdf_client = RDFClient()
     rdfs = rdf_client.get_rdf_from_id(obj_id, format, oms_crud_tool)
     if not rdfs:
