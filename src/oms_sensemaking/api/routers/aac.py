ce8fc866d60b450b7c2f81640fc776cd335e4eb4
DeMario Hamilton <demario@blackcape.io>
2025-07-25 16:14:09 -0400

    Returning 400 when cache is not enabled

--- src/oms_sensemaking/api/routers/aac.py
+++ src/oms_sensemaking/api/routers/aac.py
@@ -4,15 +4,19 @@ import logging
 from fastapi import APIRouter, HTTPException, Response
 
 from oms_sensemaking.clients.instances import aac_client
+from oms_sensemaking.config import SETTINGS
 
 LOGGER: logging.Logger = logging.getLogger(__name__)
 
 router: APIRouter = APIRouter()
 
 
-@router.get("/clear")
+@router.post("/clear")
 def clear_aac_cache():
     """Clear Local AAC Cache"""
     LOGGER.info("Clearing Local AAC Cache")
-    aac_client.clear_cache()
+    if SETTINGS.aac_cache_enabled:
+        aac_client.clear_cache()
+    else:
+        raise HTTPException(400, detail="Local AAC Cache not enabled. Unable to clear cache.")
     return Response(status_code=204)
