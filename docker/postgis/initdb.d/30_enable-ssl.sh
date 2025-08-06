4a349624fead6c7828ae6b36836e66d5e30b3aba
Nick Hawkins <nick.hawkins@blackcape.io>
2025-08-06 09:50:04 -0400

    Update postgis initdb scripts to use variables dynamically allocated from docker-compose.

--- docker/postgis/initdb.d/30_enable-ssl.sh
+++ docker/postgis/initdb.d/30_enable-ssl.sh
@@ -1,22 +1,26 @@
 # 30_enable-ssl.sql --- Enable SSL with custom keypair.
 
-# Copy certs to a writable directory and set ownership/permissions
-if [ -f /opt/common/pki/server.public ] && [ -f /opt/common/pki/server.private ]; then
+#!/bin/bash
+set -e
 
+# Copy certs to a writable directory and set ownership/permissions
+if [ -f "$HOST_TLS_CERT_FILE" ] && [ -f "$HOST_TLS_KEY_FILE" ]; then
   # explicitly set ownership to match the user running postgresql/postgis
-  chown postgres: /opt/common/pki/server.public
-  chown postgres: /opt/common/pki/server.private
+  chown postgres: "$HOST_TLS_CERT_FILE"
+  chown postgres: "$HOST_TLS_KEY_FILE"
 
   # set file permissions
-  chmod 600 /opt/common/pki/server.private
-  chmod 640 /opt/common/pki/server.public
+  chmod 600 "$HOST_TLS_KEY_FILE"
+  chmod 640 "$HOST_TLS_CERT_FILE"
+  echo "==> [SSL] Ownership and file permissions set on certificate and key files."
 
   # configure and enable SSL
-  psql << EOF
-ALTER SYSTEM SET ssl_cert_file TO '/opt/common/pki/server.public';
-ALTER SYSTEM SET ssl_key_file TO '/opt/common/pki/server.private';
-ALTER SYSTEM SET ssl TO 'ON';
-EOF
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=postgres <<-EOSQL
+    ALTER SYSTEM SET ssl_cert_file TO '$HOST_TLS_CERT_FILE';
+    ALTER SYSTEM SET ssl_key_file TO '$HOST_TLS_KEY_FILE';
+    ALTER SYSTEM SET ssl TO 'ON';
+EOSQL
+  echo "==> [SSL] SSL configuration applied."
 else
-  echo "Both service /opt/common/pki/server.public and /opt/common/pki/server.private are required for SSL" 1>&2;
+  echo "==> [SSL] ERROR: Both $HOST_TLS_CERT_FILE and $HOST_TLS_KEY_FILE are required for SSL" 1>&2
 fi
