c37813d490d637eeb81869efd8e99aed2e876aac
DeMario Hamilton <demario@blackcape.io>
2025-08-08 10:31:23 -0400

    ruff format messes up the tests

--- tests/core/test_error_loggers.py
+++ tests/core/test_error_loggers.py
@@ -82,7 +82,7 @@ def test_rethrow_db_error_logging(mock_db_session: Session, ts_acm):
         object_type="NODE",
         event_type="CREATE",
         module_name=__file__,
-        line_no=103,  # from the line above in test_error
+        line_no=101,  # from the line above in test_error
         function_name="test_rethrow_db_error_logging",
         code="_ = foo.fake_attr",
         exception_name="AttributeError",
