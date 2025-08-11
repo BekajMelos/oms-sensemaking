7c259eee73db2c66a5e2458536a9796e1518706d
sereno <sereno.dominguez@blackcape.io>
2025-08-11 13:47:40 -0400

    fix prev_status value

--- src/oms_sensemaking/iw/sensemakers/observables/base_observable.py
+++ src/oms_sensemaking/iw/sensemakers/observables/base_observable.py
@@ -160,24 +160,24 @@ class BaseObservable(BaseModel):
         LOGGER.info(f"Observed {num_observed} of {total} objects which met criteria for observable {self.id}")
 
         prev_status = self.get_status_attr()
-        new_status = self.determine_status(num_observed, total)
+        prev_status_value = prev_status.attributeValue if prev_status else ""
+        new_status_value = self.determine_status(num_observed, total)
 
-        if prev_status != new_status:
+        if prev_status_value != new_status_value:
             self.oms_client.update_attribute(  # pyright: ignore[reportOptionalMemberAccess] - ensure_initialized has been called
                 UpdateAttributeInput(
                     id=self.status_attribute_id,
-                    attributeValue=new_status,
-                    attributeDisplayValue=new_status,
-                    attributeNormalizedValue=new_status,
+                    attributeValue=new_status_value,
+                    attributeDisplayValue=new_status_value,
+                    attributeNormalizedValue=new_status_value,
                     isUserEntered=False,
                     labels=[SETTINGS.sm_inferenced_label],
                 )
             )
 
             LOGGER.info(
-                f"Updated status{" from " + prev_status.attributeValue if prev_status  else ""}"
-                f" to {new_status} for observable {self.id}"
+                f"Updated status{" from " + prev_status_value}" f" to {new_status_value} for observable {self.id}"
             )
 
         else:
-            LOGGER.info(f"Status for observable {self.id} remained as {prev_status}")
+            LOGGER.info(f"Status for observable {self.id} remained as {prev_status_value}")
