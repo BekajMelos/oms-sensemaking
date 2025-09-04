c858829e423151fd3d504e668ba1a7b697f7a8d0
Aaron Ko <aaron.ko@blackcape.io>
2025-09-04 16:38:59 -0400

    reverting to using directory base

--- tests/domain/area_of_interest/test_aoi_extractor.py
+++ tests/domain/area_of_interest/test_aoi_extractor.py
@@ -5,12 +5,7 @@ from oms_sensemaking.domain.area_of_interest.base import AOIExtractor
 
 class FakeAOIExtractor(AOIExtractor):
     def __init__(self):
-        self.path = [
-            "tests/unit_test_data/aoi/test_aoi.kml",
-            "tests/unit_test_data/aoi/test_aoi1.json",
-            "tests/unit_test_data/aoi/test_aoi2.json",
-            "tests/unit_test_data/aoi/test_zip_aoi.kmz",
-        ]
+        self.path = "tests/unit_test_data/aoi"
 
     def get_areas_of_interest(self):
         raw_features = gather_area_of_interest_data(self.path)
