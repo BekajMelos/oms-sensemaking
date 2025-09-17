abde26d57fc091c7fe0de7ad65322bf43c4dd010
David Nyman <david.nyman@blackcape.io>
2025-09-17 06:41:44 -0400

    Create units and source

--- scripts/load_test/main.py
+++ scripts/load_test/main.py
@@ -5,23 +5,50 @@ from oms_sdk.generated.generated_graphql_client import (
     Confidence,
     CreateAttributeInput,
     CreateNodeInput,
+    CreateOriginatorInput,
+    CreateProviderInput,
+    CreateSourceInput,
     Domain,
+    NodeNode,
     ObjectTier,
+    OriginatorOriginator,
+    OriginatorQuery,
+    ProviderProvider,
+    ProviderQuery,
+    SourceQuery,
+    SourceSource,
 )
+from oms_sdk.generated.generated_graphql_client.exceptions import GraphQLClientGraphQLMultiError
 
 from oms_sensemaking.config import SETTINGS
+
+omsb_url = "https://localhost:8020/graphql"
 
 
 class AtomsClient:
     def __init__(self, user_dn=""):
         self.client: Client = get_generated_graphql_client(
-            url=SETTINGS.omsb_url,
+            url=omsb_url,
             user_dn=user_dn or SETTINGS.user_dn,
             cert_path=SETTINGS.cert_path,
             key_path=SETTINGS.key_path,
             pkcs12_path=SETTINGS.pkcs12_path,
             pkcs12_password=SETTINGS.pkcs12_password,
         )
+
+
+atoms_client = AtomsClient()
+
+
+class Sourcing:
+    originator: OriginatorOriginator
+    provider: ProviderProvider
+    source: SourceSource
+
+    def __init__(self, originator, provider, source) -> None:
+        self.originator = originator
+        self.provider = provider
+        self.source = source
 
 
 class Location:
@@ -44,24 +71,24 @@ class DTO:
         self.client = client
 
 
-class Unit:
-    def __init__(self, name, id=""):
-        self.name = name
-        self.id = id
+class Unit(NodeNode):
+    def __init__(self):
+        pass
 
 
-class UnitDTO:
-    def create(self, unit):
+class UnitService(DTO):
+    def create(self, name):
         node_input = CreateNodeInput(
-            name=unit.name,
+            name=name,
             acm=DEFAULT_ACM,
-            tier=ObjectTier.primary,
+            tier=ObjectTier.PRIMARY,
             domain=Domain.AIR,
             tags=["Load Test"],
-            labels=["Load Test"],
+            labels=[],
             classIri="http://www.ontologyrepository.com/CommonCoreOntologies/Aircraft",
+            allegiance="USA",
         )
-        unit = self.client.client.create_node(node_input)
+        return self.client.client.create_node(node_input)
 
 
 class Relationship:
@@ -86,15 +113,15 @@ class Garrison:
         self.id = id
 
 
-class GarrisonDTO(DTO):
-    def create(self, garrison: Garrison):
+class GarrisonService(DTO):
+    def create(self, name, location: Location, sourcing: Sourcing):
         node_input = CreateNodeInput(
-            name=garrison.name,
+            name=name,
             acm=DEFAULT_ACM,
-            tier=ObjectTier.primary,
+            tier=ObjectTier.PRIMARY,
             domain=Domain.GROUND,
             tags=["Load Test"],
-            labels=["Load Test"],
+            labels=[],
             classIri="https://foundry.ai.mil/ontology/4901-001/Facility",
         )
 
@@ -104,10 +131,11 @@ class GarrisonDTO(DTO):
             nodeId=self._node_data.id,
             attributeIri=SETTINGS.inference_geo_attribute_iri,
             attributeType=AttributeType.GEOSPATIAL,
-            geometry=garrison.location.geometry(),
+            geometry=location.geometry(),
             valueStart="2023-01-01T00:00:00+00:00",
             valueEnd="2028-01-01T00:00:00+00:00",
             confidence=Confidence.LOW,
+            sourceId=sourcing.source.id,
         )
         self.client.client.create_attribute(attr_input)
 
@@ -118,10 +146,60 @@ class DetermineOutOfGarrison:
 
 
 def create_units(limit):
-    units = []
+    unit_names = []
+    unit_service = UnitService(atoms_client)
     for index in range(limit):
-        units.append(Unit(f"Unit no. {index}"))
+        unit_names.append(f"Unit no. {index}")
+
+    units = []
+    for unit_name in unit_names:
+        unit = unit_service.create(unit_name)
+        units.append(unit)
+        print(f"created unit {unit.id}: {unit.name}")
+
+
+def create_sourcing() -> Sourcing:
+    try:
+        originator_input = CreateOriginatorInput(name="originator", description="descr", tags=[], acm=DEFAULT_ACM)
+        orig = atoms_client.client.create_originator(originator_input)
+    except GraphQLClientGraphQLMultiError as e:
+        if len(e.errors) == 1 and e.errors[0].message == "An object already exists with the given unique key":
+            orig = atoms_client.client.originators(OriginatorQuery()).data[0]
+        else:
+            raise e
+    try:
+        provider_input = CreateProviderInput(
+            name="provider", description="descr", originatorId=orig.id, acm=DEFAULT_ACM
+        )
+        prov = atoms_client.client.create_provider(provider_input)
+    except GraphQLClientGraphQLMultiError as e:
+        if len(e.errors) == 1 and e.errors[0].message == "An object already exists with the given unique key":
+            prov = atoms_client.client.providers(ProviderQuery()).data[0]
+        else:
+            raise e
+    try:
+        source_input = CreateSourceInput(
+            name="source",
+            providerId=prov.id,
+            acm=DEFAULT_ACM,
+            identifier="https://dev.com",
+            dataAcm=DEFAULT_ACM,
+            dateOfReport="2023-01-01T00:00:00+00:00",
+            dateOfInformation="2023-01-01T00:00:00+00:00",
+        )
+        src = atoms_client.client.create_source(source_input)
+    except GraphQLClientGraphQLMultiError as e:
+        if len(e.errors) == 1 and e.errors[0].message == "An object already exists with the given unique key":
+            src = atoms_client.client.originators(SourceQuery()).data[0]
+        else:
+            raise e
+
+    print(f"Using source {src.id}")
+
+    return Sourcing(orig, prov, src)
 
 
 if __name__ == "__main__":
     print("running load test")
+    create_units(10)
+    sourcing: Sourcing = create_sourcing()
