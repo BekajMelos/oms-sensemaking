8295d296497e9e31910f3fee920b248a8b49a4f4
Aaron Ko <aaron.ko@blackcape.io>
2025-09-23 13:14:49 -0400

    made test objects as fixtures

--- tests_int/core/test_oms_crud.py
+++ tests_int/core/test_oms_crud.py
@@ -1,3 +1,4 @@
+import pytest
 from oms_sdk import DEFAULT_ACM
 from oms_sdk.generated.generated_graphql_client import (
     AttributeQuery,
@@ -21,89 +22,140 @@ from oms_sensemaking.core.oms_crud import OmsCrudTool
 oms_crud_tool = OmsCrudTool()
 person_node_iri = "http://www.ontologyrepository.com/CommonCoreOntologies/Aircraft"
 work_for_relationship_iri = SETTINGS.resolution_relationship_iri
-test_node_name = "test_node_name"
-test_attribute_name = "test_attribute_name"
-test_relationship_name = "test_relationship_name"
+test_node_name1 = "test_node_name1"
+test_node_name2 = "test_node_name2"
+test_attribute_name1 = "test_attribute_name1"
+test_attribute_name2 = "test_attribute_name2"
+test_relationship_name1 = "test_relationship_name1"
+test_relationship_name2 = "test_relationship_name2"
 
 
-def test_multi_crud_operations(create_source):
-    nodes_to_publish = []
-    for i in range(2):
-        nodes_to_publish.append(
-            CreateNodeInput(
-                acm=DEFAULT_ACM,
-                name=test_node_name + f"_{i}",
-                tier=ObjectTier.DERIVATIVE,
-                tags=SETTINGS.inference_tags,
-                classIri=person_node_iri,
-                isNso=True,
-            )
-        )
-    nodes = oms_crud_tool.publish_nodes(nodes_to_publish)
-    assert len(nodes) == 2
-
-    rels_to_publish = []
-    for i in range(2):
-        rels_to_publish.append(
-            CreateRelationshipInput(
-                name=test_relationship_name + f"_{i}",
-                startNodeId=nodes[0].id,
-                endNodeId=nodes[1].id,
-                sourceId=create_source.id,
-                confidence=Confidence.UNKNOWN,
-                acm=DEFAULT_ACM,
-                objectPropertyIri=work_for_relationship_iri,
-            )
-        )
-    rels = oms_crud_tool.publish_relationships(rels_to_publish)
-    assert len(rels) == 2
-
-    attrs_to_publish = []
-    for i in range(2):
-        attrs_to_publish.append(
-            CreateAttributeInput(
-                attributeIri=SETTINGS.mil_symbol_settings.symbol_attribute_iri,
-                attributeValue=test_attribute_name + f"_{i}",
-                attributeType=AttributeType.STRING,
-                confidence=Confidence.UNKNOWN,
-                sourceId=create_source.id,
-                nodeId=nodes[0].id,
-                acm=DEFAULT_ACM,
-            )
-        )
-    attributes = oms_crud_tool.publish_attributes(attrs_to_publish)
-    assert len(attributes) == 2
-
-    # Cleanup test data
-    for attribute in attributes:
-        delete_attrs = oms_crud_tool.delete_attribute(attribute_id=attribute.id)
-        assert delete_attrs
-    for relationship in rels:
-        delete_rels = oms_crud_tool.delete_relationship(relationship_id=relationship.id)
-        assert delete_rels
-    for node in nodes:
-        delete_nodes = oms_crud_tool.delete_node(node_id=node.id)
-        assert delete_nodes
-
-
-def test_node_crud():
-    # Create test
-    node = oms_crud_tool.create_node(
+@pytest.fixture(scope="function")
+def test_node1():
+    node1 = oms_crud_tool.create_node(
         CreateNodeInput(
             acm=DEFAULT_ACM,
-            name=test_node_name,
+            name=test_node_name1,
             tier=ObjectTier.DERIVATIVE,
             tags=SETTINGS.inference_tags,
             classIri=person_node_iri,
             isNso=True,
         )
     )
+    yield node1
+    delete_node1 = oms_crud_tool.delete_node(node1.id)
+    assert delete_node1
+
+
+@pytest.fixture(scope="function")
+def test_node2():
+    node2 = oms_crud_tool.create_node(
+        CreateNodeInput(
+            acm=DEFAULT_ACM,
+            name=test_node_name2,
+            tier=ObjectTier.DERIVATIVE,
+            tags=SETTINGS.inference_tags,
+            classIri=person_node_iri,
+            isNso=True,
+        )
+    )
+    yield node2
+    delete_node2 = oms_crud_tool.delete_node(node_id=node2.id)
+    assert delete_node2
+
+
+@pytest.fixture(scope="function")
+def test_relationship1(create_source, test_node1, test_node2):
+    rel1 = oms_crud_tool.create_relationship(
+        CreateRelationshipInput(
+            name=test_relationship_name1,
+            startNodeId=test_node1.id,
+            endNodeId=test_node2.id,
+            sourceId=create_source.id,
+            confidence=Confidence.UNKNOWN,
+            acm=DEFAULT_ACM,
+            objectPropertyIri=work_for_relationship_iri,
+        )
+    )
+    yield rel1
+    delete_rel1 = oms_crud_tool.delete_relationship(relationship_id=rel1.id)
+    assert delete_rel1
+
+
+@pytest.fixture(scope="function")
+def test_relationship2(create_source, test_node1, test_node2):
+    rel2 = oms_crud_tool.create_relationship(
+        CreateRelationshipInput(
+            name=test_relationship_name2,
+            startNodeId=test_node1.id,
+            endNodeId=test_node2.id,
+            sourceId=create_source.id,
+            confidence=Confidence.UNKNOWN,
+            acm=DEFAULT_ACM,
+            objectPropertyIri=work_for_relationship_iri,
+        )
+    )
+    yield rel2
+    delete_rel2 = oms_crud_tool.delete_relationship(relationship_id=rel2.id)
+    assert delete_rel2
+
+
+@pytest.fixture(scope="function")
+def test_attr1(create_source, test_node1):
+    attr1 = oms_crud_tool.create_attribute(
+        CreateAttributeInput(
+            attributeIri=SETTINGS.mil_symbol_settings.symbol_attribute_iri,
+            attributeValue=test_attribute_name1,
+            attributeType=AttributeType.STRING,
+            confidence=Confidence.UNKNOWN,
+            sourceId=create_source.id,
+            nodeId=test_node1.id,
+            acm=DEFAULT_ACM,
+        )
+    )
+    yield attr1
+    delete_attr1 = oms_crud_tool.delete_attribute(attribute_id=attr1.id)
+    assert delete_attr1
+
+
+@pytest.fixture(scope="function")
+def test_attr2(create_source, test_node1):
+    attr2 = oms_crud_tool.create_attribute(
+        CreateAttributeInput(
+            attributeIri=SETTINGS.mil_symbol_settings.symbol_attribute_iri,
+            attributeValue=test_attribute_name2,
+            attributeType=AttributeType.STRING,
+            confidence=Confidence.UNKNOWN,
+            sourceId=create_source.id,
+            nodeId=test_node1.id,
+            acm=DEFAULT_ACM,
+        )
+    )
+    yield attr2
+    delete_attr2 = oms_crud_tool.delete_attribute(attribute_id=attr2.id)
+    assert delete_attr2
+
+
+def test_multi_crud_operations(test_node1, test_node2, test_relationship1, test_relationship2, test_attr1, test_attr2):
+    assert test_node1
+    assert test_node2
+
+    assert test_relationship1
+    assert test_relationship2
+
+    assert test_attr1
+    assert test_attr2
+
+
+def test_node_crud(test_node1):
+    # Create test
+    node = test_node1
     assert node
 
     # Get test
-    get_nodes = oms_crud_tool.get_nodes(NodeQuery(name=StringQuery(equals=test_node_name)))
+    get_nodes = oms_crud_tool.get_nodes(NodeQuery(name=StringQuery(equals=test_node_name1)))
     for ind_node in get_nodes.data:
-        assert ind_node.name == test_node_name
+        assert ind_node.name == test_node_name1
 
     # Update test
     new_node_name = "new_node_name"
@@ -111,46 +163,20 @@ def test_node_crud():
     assert update_node
     assert update_node.name == new_node_name
 
-    # Delete test
-    delete_node = oms_crud_tool.delete_node(node_id=node.id)
-    assert delete_node
 
-
-def test_relationship_crud(create_source):
+def test_relationship_crud(test_node1, test_node2, test_relationship1):
     # First need to create a couple nodes that the relationship can use
-    nodes_to_publish = []
-    for i in range(2):
-        nodes_to_publish.append(
-            CreateNodeInput(
-                acm=DEFAULT_ACM,
-                name=test_node_name + f"_{i}",
-                tier=ObjectTier.DERIVATIVE,
-                tags=SETTINGS.inference_tags,
-                classIri=person_node_iri,
-                isNso=True,
-            )
-        )
-    nodes = oms_crud_tool.publish_nodes(nodes_to_publish)
-    assert len(nodes) == 2
+    assert test_node1
+    assert test_node2
 
     # Create test
-    rel = oms_crud_tool.create_relationship(
-        CreateRelationshipInput(
-            name=work_for_relationship_iri,
-            startNodeId=nodes[0].id,
-            endNodeId=nodes[1].id,
-            sourceId=create_source.id,
-            confidence=Confidence.UNKNOWN,
-            acm=DEFAULT_ACM,
-            objectPropertyIri=work_for_relationship_iri,
-        )
-    )
+    rel = test_relationship1
     assert rel
 
     # Get test
-    get_rels = oms_crud_tool.get_relationships(RelationshipQuery(name=StringQuery(equals=test_relationship_name)))
+    get_rels = oms_crud_tool.get_relationships(RelationshipQuery(name=StringQuery(equals=test_relationship_name1)))
     for ind_rel in get_rels.data:
-        assert ind_rel.name == test_relationship_name
+        assert ind_rel.name == test_relationship_name1
 
     # Update test
     new_relationship_name = "new_relationship_name"
@@ -160,56 +186,20 @@ def test_relationship_crud(create_source):
     assert update_relationship
     assert update_relationship.name == new_relationship_name
 
-    # Delete test
-    delete_relationship = oms_crud_tool.delete_relationship(relationship_id=rel.id)
-    assert delete_relationship
 
-    # Cleanup test data
-    for node in nodes:
-        delete_nodes = oms_crud_tool.delete_node(node_id=node.id)
-        assert delete_nodes
-
-
-def test_attribute_crud(create_source):
-    node = oms_crud_tool.create_node(
-        CreateNodeInput(
-            acm=DEFAULT_ACM,
-            name=test_node_name,
-            tier=ObjectTier.DERIVATIVE,
-            tags=SETTINGS.inference_tags,
-            classIri=person_node_iri,
-            isNso=True,
-        )
-    )
+def test_attribute_crud(test_node1, test_attr1):
+    node = test_node1
     assert node
 
-    attribute = oms_crud_tool.create_attribute(
-        CreateAttributeInput(
-            attributeIri=SETTINGS.mil_symbol_settings.symbol_attribute_iri,
-            attributeValue=test_attribute_name,
-            attributeType=AttributeType.STRING,
-            confidence=Confidence.UNKNOWN,
-            sourceId=create_source.id,
-            nodeId=node.id,
-            acm=DEFAULT_ACM,
-        )
-    )
+    attribute = test_attr1
     assert attribute
 
     # Get test
-    get_attrs = oms_crud_tool.get_attributes(AttributeQuery(attributeValue=StringQuery(equals=test_attribute_name)))
+    get_attrs = oms_crud_tool.get_attributes(AttributeQuery(attributeValue=StringQuery(equals=test_attribute_name1)))
     for ind_attr in get_attrs.data:
-        assert ind_attr.attributeValue == test_attribute_name
+        assert ind_attr.attributeValue == test_attribute_name1
 
     # Update test
     update_attribute = oms_crud_tool.update_attribute(UpdateAttributeInput(id=attribute.id, confidence=Confidence.LOW))
     assert update_attribute
     assert update_attribute.confidence == Confidence.LOW
-
-    # Delete test
-    delete_attribute = oms_crud_tool.delete_attribute(attribute_id=attribute.id)
-    assert delete_attribute
-
-    # Cleanup test data
-    delete_nodes = oms_crud_tool.delete_node(node_id=node.id)
-    assert delete_nodes
