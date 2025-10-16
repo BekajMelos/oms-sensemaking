6470a13c03c2eba64593125a7636730e77ea8f81
Aaron Ko <aaron.ko@blackcape.io>
2025-10-16 17:58:39 -0400

    start adding api endpoint for audit log error

---
+++ src/oms_sensemaking/api/routers/audit_log_error.py
@@ -0,0 +1,25 @@
+"""Rest Endpoint for Audit Log Error"""
+
+import logging
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, Response
+
+from oms_sensemaking.api.routers.utils import check_user_dn_in_whitelist
+from oms_sensemaking.clients.instances import aac_client
+from oms_sensemaking.config import SETTINGS
+
+LOGGER: logging.Logger = logging.getLogger(__name__)
+
+router: APIRouter = APIRouter()
+
+
+@router.post("/audit")
+def get_audit_log_errors(user_dn: Annotated[str, Depends(check_user_dn_in_whitelist)]) -> Response:
+    """Clear Local AAC Cache. Available to Authorized Users Only"""
+    LOGGER.info("Clearing Local AAC Cache. User %s", user_dn)
+    if SETTINGS.aac_cache_enabled:
+        aac_client.clear_cache()
+    else:
+        raise HTTPException(400, detail="Local AAC Cache not enabled. Unable to clear cache.")
+    return Response(status_code=204)
