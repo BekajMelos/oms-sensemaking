9514d3f978f50f1b41eb4d965d2bca2534cb2ddb
Aaron Ko <aaron.ko@blackcape.io>
2025-10-20 16:29:34 -0400

    remove unneeded imports and fields

--- src/oms_sensemaking/clients/audit_log_error_client.py
+++ src/oms_sensemaking/clients/audit_log_error_client.py
@@ -3,13 +3,10 @@
 import logging
 from typing import Optional
 
-import oms_sensemaking
 from oms_sensemaking.clients.instances import db_session
 from oms_sensemaking.models.logs import AuditLogError
 
 LOGGER = logging.getLogger(__name__)
-
-SENSEMAKING_MODULE = oms_sensemaking.__name__
 
 
 class AuditLogErrorClient:
