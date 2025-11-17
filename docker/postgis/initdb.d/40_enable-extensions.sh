fb21e72ebcf44ba24fbbcc1f4c799f5e5d605463
Nick Hawkins <nick.hawkins@blackcape.io>
2025-11-17 11:12:01 -0500

    Enable pg_stat_statements extension in Postgres for performance tracking

--- docker/postgis/initdb.d/40_enable-extensions.sh
+++ docker/postgis/initdb.d/40_enable-extensions.sh
@@ -1,6 +1,7 @@
 # 40_enable-extensions.sh
 # --- Enable OMS's WFS to use hash store for Attributes
 # --- Enable OMS to match on Trigrams
+# --- Enable Postgres performance extension pg_stat_statements
 # !/bin/bash
 set -e
 
@@ -16,3 +17,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$DB_NAME_OMSB" <<-
 EOSQL
 
 echo "==> [EXTENSIONS] pg_trgm extension enabled in $DB_NAME_OMSB."
+
+# Enable pg_stat_statements
+echo "shared_preload_libraries = 'pg_stat_statements'" >> "$PGDATA/postgresql.conf"
+echo "pg_stat_statements.max = 10000" >> "$PGDATA/postgresql.conf"
+echo "pg_stat_statements.track = all" >> "$PGDATA/postgresql.conf"
