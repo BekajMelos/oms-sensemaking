d8277bb9308ba30687783960f4277fe1c53c3e8a
Nick Hawkins <nick.hawkins@blackcape.io>
2025-11-17 11:23:30 -0500

    Add CREATE EXTENSION command to sensemaking db.

--- docker/postgis/initdb.d/40_enable-extensions.sh
+++ docker/postgis/initdb.d/40_enable-extensions.sh
@@ -22,3 +22,9 @@ echo "==> [EXTENSIONS] pg_trgm extension enabled in $DB_NAME_OMSB."
 echo "shared_preload_libraries = 'pg_stat_statements'" >> "$PGDATA/postgresql.conf"
 echo "pg_stat_statements.max = 10000" >> "$PGDATA/postgresql.conf"
 echo "pg_stat_statements.track = all" >> "$PGDATA/postgresql.conf"
+
+# Enable pg_stat_statements in the database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$DB_NAME" <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL
+echo "==> [EXTENSIONS] pg_stat_statements extension enabled in $DB_NAME."
