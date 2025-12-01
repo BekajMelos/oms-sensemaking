9bfab0e9f4c0bf840150144c284e92c0016c6c20
martin.papa <martin.papa@blackcape.io>
2025-12-01 13:35:16 -0500

    Decouple JSON file access into a dedicated static utility method.

--- src/oms_sensemaking/resolution/controllers.py
+++ src/oms_sensemaking/resolution/controllers.py
@@ -37,17 +37,21 @@ class ResolutionSensemakerController(SensemakerController):
 
 class ResolutionQueueFilter(EventFilter):
     def __init__(self) -> None:
-        self.attribute_iris: list[str] = []
-        with open(SETTINGS.duplicate_object_iris_file_path) as fd:
-            duplicate_object_iris = json.load(fd)
-            self.attribute_iris = list(chain.from_iterable(duplicate_object_iris.values()))
+        duplicate_object_iris = ResolutionJsonHandler.read_json(SETTINGS.duplicate_object_iris_file_path)
+        self.attribute_iris: list[str] = list(chain.from_iterable(duplicate_object_iris.values()))
 
     def passes_filter(self, audit_log_event: AuditLogEvent) -> bool:
         handled_object_types = [ObjectType.ATTRIBUTE.value]
         handled_event_types = [Action.CREATE.value, Action.RESTORE.value]
-
         return (
             audit_log_event.objectType in handled_object_types
             and audit_log_event.action in handled_event_types
             and audit_log_event.headers.iri in self.attribute_iris
         )
+
+
+class ResolutionJsonHandler:
+    @staticmethod
+    def read_json(json_file_path: str):
+        with open(json_file_path) as json_data:
+            return json.load(json_data)
