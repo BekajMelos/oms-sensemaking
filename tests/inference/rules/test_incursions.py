2a98cd66254e6d4acce31d9c8a6d7da27c692e43
Aaron Ko <aaron.ko@blackcape.io>
2025-12-16 11:13:23 -0500

    fixed tests

--- tests/inference/rules/test_incursions.py
+++ tests/inference/rules/test_incursions.py
@@ -453,22 +453,21 @@ def test_two_existing_incursions(
             geometry=GeoQuery(queryGeoJson=areas_of_interest[0].geometry_dict, queryType=GeoQueryType.DISJOINT),
         )
     )
-    mock_crud_tool.update_attribute.assert_called_with(
-        UpdateAttributeInput(
-            id=attribute1.id,
-            valueStart=observational_node_region1.startTime,
-            valueEnd=observational_node_region1.endTime,
-            labels=[SETTINGS.sm_enriched_label],
-        )
-    )
-    mock_crud_tool.update_activity.assert_called_with(
+
+    mock_crud_tool.update_incursion_activity_and_attributes.assert_called_with(
         UpdateActivityInput(
             id="incActi1",
             observationIds=UpdateUuidList(add=[observational_node_region1.id]),
             startTime=observational_node_region1.startTime,
             endTime=observational_node_region1.endTime,
             labels=[SETTINGS.sm_enriched_label],
-        )
+        ),
+        UpdateAttributeInput(
+            id=attribute1.id,
+            valueStart=observational_node_region1.startTime,
+            valueEnd=observational_node_region1.endTime,
+            labels=[SETTINGS.sm_enriched_label],
+        ),
     )
 
 
@@ -502,7 +501,14 @@ def test_existing_incursion_nonoverlapping_time(
             geometry=GeoQuery(queryGeoJson=areas_of_interest[0].geometry_dict, queryType=GeoQueryType.DISJOINT),
         )
     )
-    mock_crud_tool.update_attribute.assert_called_with(
+    mock_crud_tool.update_incursion_activity_and_attributes.assert_called_with(
+        UpdateActivityInput(
+            id="incActi1",
+            observationIds=UpdateUuidList(add=[observational_node_region1.id]),
+            startTime=attribute2.valueStart,
+            endTime=observational_node_region1.endTime,
+            labels=[SETTINGS.sm_enriched_label],
+        ),
         UpdateAttributeInput(
             id=attribute2.id,
             valueStart=attribute2.valueStart,
@@ -510,16 +516,7 @@ def test_existing_incursion_nonoverlapping_time(
             labels=[
                 SETTINGS.sm_enriched_label,
             ],
-        )
-    )
-    mock_crud_tool.update_activity.assert_called_with(
-        UpdateActivityInput(
-            id="incActi1",
-            observationIds=UpdateUuidList(add=[observational_node_region1.id]),
-            startTime=attribute2.valueStart,
-            endTime=observational_node_region1.endTime,
-            labels=[SETTINGS.sm_enriched_label],
-        )
+        ),
     )
 
 
