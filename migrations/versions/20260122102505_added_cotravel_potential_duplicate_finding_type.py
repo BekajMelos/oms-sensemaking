012d0b25eefba4b0e8cb2639cbd5a701a7de5334
Nikolas Nash <nikolas.nash@blackcape.io>
2026-01-22 10:28:54 -0500

    fixed alembic

--- migrations/versions/20260122102505_added_cotravel_potential_duplicate_finding_type.py
+++ migrations/versions/20260122102505_added_cotravel_potential_duplicate_finding_type.py
@@ -1,8 +1,8 @@
 """Added new Cotravel Potential Duplicate Finding Type.
 
-Revision ID: 202601151616
-Revises: 202512101040
-Create Date: 2026-01-15 16:16:00.000000
+Revision ID: 20260122102505
+Revises: 202601161555
+Create Date: 2026-01-22 10:25:05.000000
 
 """
 
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "202601151616"
-down_revision: Union[str, None] = "202512101040"
+revision: str = "20260122102505"
+down_revision: Union[str, None] = "202601161555"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
