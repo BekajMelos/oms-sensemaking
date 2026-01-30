ca114cafdf32d9e74e76d80ce8e5f35a1afb5b8b
martin.papa <martin.papa@blackcape.io>
2026-01-29 23:27:32 -0500

    Revert "remove rules_context, and associated test controller"
    This reverts commit 00404cfde09ce6d8074229832460a67a30a671f0.

---
+++ src/oms_sensemaking/inference/rules/rule_context.py
@@ -0,0 +1,24 @@
+from oms_sdk.generated.generated_graphql_client.activity import ActivityActivity
+from oms_sdk.generated.generated_graphql_client.attribute import AttributeAttribute
+from oms_sdk.generated.generated_graphql_client.node import NodeNode
+from oms_sdk.generated.generated_graphql_client.observation import ObservationObservation
+from oms_sdk.generated.generated_graphql_client.relationship import RelationshipRelationship
+from oms_sdk.generated.generated_graphql_client.source import SourceSource
+
+
+class RuleContext:
+    """
+    Map inputs for the rules into well defined models, this gives us direct access
+    to properties in the rules for a variety of object types
+    """
+
+    def __init__(self, **kwargs) -> None:
+        self.attribute: AttributeAttribute = kwargs.get("attribute")
+        self.relationship: RelationshipRelationship = kwargs.get("relationship")
+        self.source: SourceSource = kwargs.get("source")
+        self.node: NodeNode = kwargs.get("node")
+        self.observation: ObservationObservation = kwargs.get("observation")
+        self.activity: ActivityActivity = kwargs.get("observation")
+
+    def get_properties(self):
+        return vars(self)
