5683f94eaac41d981b4de5130d11fc1bcd4ad7c2
martin.papa <martin.papa@blackcape.io>
2026-02-05 14:07:51 -0500

    fix formatting

--- src/oms_sensemaking/core/oms_crud.py
+++ src/oms_sensemaking/core/oms_crud.py
@@ -1,6 +1,6 @@
 import logging
 import warnings
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 from uuid import UUID
 
 from cachetools import TTLCache, cached
@@ -340,14 +340,14 @@ class OmsCrudTool(BaseClient):
         :param object_id: Id of ATOMS object to retrieve
         :param object_type: ObjectType type of object to retrieve
         """
-        obj_getter_mapping: Dict[ObjectType, UUID] = {
+        obj_getter_mapping: Dict[ObjectType, Any] = {
             ObjectType.ACTIVITY: self.get_activity,
             ObjectType.ATTRIBUTE: self.get_attribute,
             ObjectType.OBSERVATION: self.get_observation,
             ObjectType.NODE: self.get_node,
             ObjectType.RELATIONSHIP: self.get_relationship,
         }
-        print(f"obj_getter_mapping: {obj_getter_mapping}")
+
         return obj_getter_mapping[object_type](object_id)
 
     def get_ontology_class(self, iri: str) -> Optional[OntologyClassOntologyClass]:
