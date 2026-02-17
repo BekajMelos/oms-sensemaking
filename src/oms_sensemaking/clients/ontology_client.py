018d6b3bf843fe41202a69d55c59b8050b62ab17
Nikolas Nash <nikolas.nash@blackcape.io>
2026-02-17 14:32:46 -0500

    checkpoint

--- src/oms_sensemaking/clients/ontology_client.py
+++ src/oms_sensemaking/clients/ontology_client.py
@@ -45,7 +45,8 @@ class OntologyClient(OntologyService):
         :return: The Node's ancestor's iri list
         """
 
-        iris = set()
+        iris: set[str] = set()
+        visited: set[str] = {oms_node.classIri}  # include start so we detect cycle back to it
         iris_to_check: SimpleQueue = SimpleQueue()
         iris_to_check.put_nowait(oms_node.classIri)
         while not iris_to_check.empty():
@@ -56,8 +57,12 @@ class OntologyClient(OntologyService):
                 continue
 
             for parent_ontology_class in ontology_class.parentOntologyClasses:
-                iris.add(parent_ontology_class.iri)
-                iris_to_check.put_nowait(parent_ontology_class.iri)
+                parent_iri = parent_ontology_class.iri
+                if parent_iri in visited:
+                    continue
+                visited.add(parent_iri)
+                iris.add(parent_iri)
+                iris_to_check.put_nowait(parent_iri)
 
         return iris
 
@@ -70,10 +75,10 @@ class OntologyClient(OntologyService):
 
         # OMSB currently does not return the ancestorOntologyClasses in order so we have to query manually for now
 
-        iris = []
-        has_parent = True
+        iris: list[str] = []
+        visited: set[str] = {oms_node.classIri}
         current_iri = oms_node.classIri
-        while has_parent:
+        while True:
             ontology_class: Optional[OntologyClassOntologyClass] = self.get_ontology_class(iri=current_iri)
 
             if not ontology_class or not ontology_class.parentOntologyClasses:
@@ -81,17 +86,25 @@ class OntologyClient(OntologyService):
 
             # If multiple parent Iris, just get the first one
             parent_iri = ontology_class.parentOntologyClasses[0].iri
+            if parent_iri in visited:
+                break
+            visited.add(parent_iri)
             iris.append(parent_iri)
             current_iri = parent_iri
 
         return iris
 
-    def get_default_symbol_id_code(self, iri: str) -> Optional[str]:
+    def get_default_symbol_id_code(self, iri: str, _visited: Optional[set[str]] = None) -> Optional[str]:
         """Given an iri, return the closest parent with a defaultSymbolIdCode
 
         :param iri: Iri to search for
         :return: Closest parent iri with a defaultSymbolIdCode
         """
+        if _visited is None:
+            _visited = set()
+        if iri in _visited:
+            return None
+        _visited.add(iri)
 
         ontology_class: Optional[OntologyClassOntologyClass] = self.get_ontology_class(iri=iri)
         if not ontology_class:
@@ -107,4 +120,4 @@ class OntologyClient(OntologyService):
         # If multiple parent Iris, just get the first one
         super_class_iri: str = ontology_class.parentOntologyClasses[0].iri
 
-        return self.get_default_symbol_id_code(super_class_iri)
+        return self.get_default_symbol_id_code(super_class_iri, _visited)
