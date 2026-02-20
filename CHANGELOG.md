f9489deafc4fbc8292accb69e23bb67e89f1e604
chris.mejia <christopher.mejia@blackcape.io>
2026-02-20 14:21:20 -0500

    SM-785-stop-using-provider-to-split-tracks (#798)
    # Pull Request
    
    * Closes #785
    
    ## Changes
    
    - Removed provider_id from the database in the tracks table.
    - Removed provider_id from the tracks model
    - Removed logic from generate_tracks that bucketed by provider_id
    
    ## Test Procedure
    Mention if there are new dependencies or resources needed to verify this branch.
    
    * checkout this branch
    * Run `source .venv/bin/activate`
    * Copy updates from `.env.template` to `.env`
    * Run `make install refresh` to apply dependency updates
    * Run `make up`
    * Run `make fix`, verify no changes
    * Run `make test`, verify no failures
    * Run `Sensemaking Smoke Tests` collection in Postman, verify no failures
    * `make pgadmin` and verify the tracks table no longer has a provider_id column
    
    ## Checklist before requesting a review
    - [ ] I have performed a self-review of my code
    - [ ] If it is a core feature, I have added thorough tests.
    - [ ] I have opened or added changes to an MR for Sensemaking in the omsb-helm-charts repo
    - [ ] I have updated environment variables in the docs
    - [ ] I've updated the changelog
    
    Co-authored-by: Chris Mejia <chrismejia@Chriss-MacBook-Pro.local>
    Reviewed-on: https://tex.gerbil-cloud.ts.net:3000/oms/oms-sensemaking/pulls/798
    Reviewed-by: david.nyman <david.nyman@blackcape.io>
    Reviewed-by: aaron.ko <aaron.ko@blackcape.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,7 +7,8 @@ All notable changes to this project will be documented in this file.
 ### Added
 
 ### Changed
-- Loiters now return activities 
+- Loiters now return activities
+- Removed provider_id from database and splitting logic
 
 ### Fixed
 - Resolved index and comment discrepancies between alembic and SQLAlchemy models.
--- src/oms_sensemaking/models/geo.py
+++ src/oms_sensemaking/models/geo.py
@@ -202,11 +202,6 @@ class Track(BaseORM, SecurityMarkingMixin):
         nullable=False,
         comment="The node ID of the object associated with this track.",
     )
-    provider_id: Mapped[uuid.UUID] = mapped_column(
-        UUID(as_uuid=True),
-        nullable=False,
-        comment="The provider ID of the provider associated with this track.",
-    )
     algorithm: Mapped[str] = mapped_column(
         String,
         nullable=True,
@@ -327,7 +322,7 @@ class TrackWeaverBase(ABC):
         self.config: dict = {}
 
     @abstractmethod
-    def execute(self, points: list[Point], provider_id: uuid.UUID) -> Track:
+    def execute(self, points: list[Point]) -> Track:
         """
         Weave a Track from a series of Points.
 
@@ -358,7 +353,7 @@ class NaiveTrackWeaver(TrackWeaverBase):
         self.config = {}
         self.algorithm = "naive"
 
-    def execute(self, points: list[Point], provider_id: uuid.UUID) -> Track:
+    def execute(self, points: list[Point]) -> Track:
         points.sort(key=lambda x: x.detection_time)
         return Track(
             points=points,
@@ -366,7 +361,6 @@ class NaiveTrackWeaver(TrackWeaverBase):
             algorithm=self.algorithm,
             observation_ids={p.observation_id for p in points},  # type: ignore
             acm=aac_client.get_acm_rollup([point.acm for point in points]),
-            provider_id=provider_id,
         )
 
 
@@ -397,7 +391,7 @@ class TimeBinTrackWeaver(TrackWeaverBase):
         }
         self.algorithm = "time_bin_weighted_average"
 
-    def execute(self, points: list[Point], provider_id: uuid.UUID) -> Track:
+    def execute(self, points: list[Point]) -> Track:
         points.sort(key=lambda x: x.detection_time)
         # Integer division by bin size sorts timestamps into bins of arbitrary length
         time_bins = {
@@ -455,7 +449,6 @@ class TimeBinTrackWeaver(TrackWeaverBase):
             algorithm=self.algorithm,
             observation_ids={p.observation_id for p in points},  # type: ignore
             acm=aac_client.get_acm_rollup([point.acm for point in weighted_points]),
-            provider_id=provider_id,
         )
 
 
