a3e26e40049e411ac0b5b61d97d65f7ef9a77a36
LekhaReddy <lekha.reddy@blackcape.io>
2026-04-02 14:52:11 -0500

    listens for node changes

--- src/oms_sensemaking/cocom/controllers.py
+++ src/oms_sensemaking/cocom/controllers.py
@@ -1,10 +1,8 @@
 """COCOM traversal sensemaker controller"""
 
 import logging
-from uuid import UUID
 
 from oms_sdk.generated.generated_graphql_client.enums import Action, ObjectType
-from oms_sdk.generated.generated_graphql_client.observation import ObservationObservation
 
 from oms_sensemaking.cocom.sensemaker import COCOMTraversalSensemaker
 from oms_sensemaking.config import SETTINGS
@@ -33,40 +31,7 @@ class COCOMTraversalSensemakerController(SensemakerController):
 
         super().start()
 
-    def handle_event(self, event: AuditLogEvent) -> bool:
-        """
-        Handle inbound ATOMS event.
-
-        :param event: The event to process.
-        :return: True if the audit log event was successfully processed, False otherwise.
-        """
-        LOGGER.debug("Received AuditLogEvent(objectId=%s)", event.objectId)
-
-        # Retrieve observation
-        oms_obs = self.get_oms_observation(event.objectId)
-
-        return bool(not oms_obs)
-
-    def get_oms_observation(self, observation_id: UUID) -> ObservationObservation | None:
-        """
-        Given an ATOMS Observation ID, get the ATOMS Observation.
-
-        :param observation_id: ID of the observation
-        :return: None if no observation exists, or the ATOMS Observation
-        """
-        # get observation
-        oms_obs: ObservationObservation = self.oms_crud_tool.get_observation(observation_id)
-
-        # Filter observations
-        # Only process if there is an observation
-        if not oms_obs:
-            return None
-
-        return oms_obs
-
 
 class COCOMTraversalQueueFilter(EventFilter):
     def passes_filter(self, audit_event: AuditLogEvent):
-        handled_object_types = [ObjectType.OBSERVATION.value]
-        handled_event_types = [Action.CREATE.value, Action.RESTORE.value, Action.UPDATE.value]
-        return audit_event.objectType in handled_object_types and audit_event.action in handled_event_types
+        return audit_event.objectType == ObjectType.NODE.value and audit_event.action == Action.UPDATE.value
