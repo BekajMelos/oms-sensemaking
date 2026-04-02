40a8cb7d83f08b66de97b2b7de73effb64803271
Melos Bekaj <melos.bekaj@blackcape.io>
2026-04-02 18:01:39 -0400

    Changed tests as well

--- tests/core/test_port_ingest.py
+++ tests/core/test_port_ingest.py
@@ -1,86 +1,48 @@
-# import xml.etree.ElementTree as ET
-# from unittest import mock
+from unittest import mock
 
-# from oms_sensemaking.core.port_ingest import import_geo_data, import_kml_data
-# from oms_sensemaking.models.port_ingest import CocomArea, CocomType
+from oms_sensemaking.core.port_ingest import import_aggressor_port_data
 
 
-# @mock.patch("oms_sensemaking.core.cocom.check_geodata", return_value=False)
-# @mock.patch("oms_sensemaking.core.cocom.json.load")
-# @mock.patch("builtins.open", new_callable=mock.mock_open)
-# @mock.patch("oms_sensemaking.core.cocom.db_session")
-# def test_import_geodata_success(mock_db_session, mock_file, mock_json_load, mock_check_geodata):
+@mock.patch("oms_sensemaking.core.port_ingest.check_ports", return_value=False)
+@mock.patch("oms_sensemaking.core.port_ingest.json.load")
+@mock.patch("builtins.open", new_callable=mock.mock_open)
+@mock.patch("oms_sensemaking.core.port_ingest.db_session")
+def test_import_geodata_success(mock_db_session, mock_json_load):
 
-#     mock_json_load.return_value = {
-#         "features": [
-#             {
-#                 "geometry": {"type": "Polygon", "coordinates": [[[0, 0], [0, 1], [1, 1], [0, 0]]]},
-#                 "properties": {"oth_fcs": "U.S. Africa Command"},
-#             }
-#         ]
-#     }
+    mock_json_load.return_value = [
+        {
+            "id": "observation-25",
+            "capco": "U",
+            "tags": "AGGRESSOR;WORLD-PORT-INDEX",
+            "labels": "",
+            "classIri": "http://www.ontologyrepository.com/CommonCoreOntologies/ObjectTrackPoint",
+            "displayValue": "The port's location.",
+            "confidence": "HIGH",
+            "sourceId": "WPI-source-0",
+            "nodeId": "node-62400.0",
+            "geometry": '{"type": "Feature", "geometry": {"type": "Point", "coordinates": [129.883333, 33.283333]}}',
+            "startTime": "2026-03-30T14:33:11Z",
+            "endTime": "2026-03-30T14:33:11Z",
+        }
+    ]
 
-#     result = import_geo_data()
+    result = import_aggressor_port_data()
 
-#     assert result is True
-#     mock_db_session.return_value.__enter__.return_value.add.assert_called_once()
-#     # Verify the object added to the session has the correct Enum value
-#     added_obj = mock_db_session.return_value.__enter__.return_value.add.call_args[0][0]
-#     assert isinstance(added_obj, CocomArea)
-#     assert added_obj.cocom_type == CocomType.AFRICOM
-#     assert mock_db_session.return_value.__enter__.return_value.commit.called
-
-
-# @mock.patch("oms_sensemaking.core.cocom.check_geodata", return_value=False)
-# @mock.patch("oms_sensemaking.core.cocom.json.load")
-# @mock.patch("builtins.open", new_callable=mock.mock_open)
-# @mock.patch("oms_sensemaking.core.cocom.db_session")
-# def test_import_geodata_error(mock_db_session, mock_file, mock_json_load, mock_check_geodata):
-
-#     mock_json_load.return_value = {"features": ["bad_json"]}
-
-#     result = import_geo_data()
-
-#     assert result is False
-#     mock_db_session.return_value.__enter__.return_value.add.assert_not_called()
-#     mock_db_session.return_value.__enter__.return_value.rollback.assert_called()
+    assert result is True
+    mock_db_session.return_value.__enter__.return_value.add.assert_called_once()
+    assert mock_db_session.return_value.__enter__.return_value.commit.called()
 
 
-# @mock.patch("oms_sensemaking.core.cocom.check_geodata", return_value=False)
-# @mock.patch("oms_sensemaking.core.cocom.ET.parse")
-# @mock.patch("oms_sensemaking.core.cocom.db_session")
-# def test_import_kml_data_success(mock_db_session, mock_et_parse, mock_check_geodata):
+@mock.patch("oms_sensemaking.core.cocom.check_geodata", return_value=False)
+@mock.patch("oms_sensemaking.core.cocom.json.load")
+@mock.patch("builtins.open", new_callable=mock.mock_open)
+@mock.patch("oms_sensemaking.core.cocom.db_session")
+def test_import_geodata_error(mock_db_session, mock_json_load):
 
-#     kml_content = """<kml><Placemark>
-#         <SimpleData name="oth_fcs">U.S. European Command</SimpleData>
-#         <Polygon><coordinates>10,20 10,21 11,21 10,20</coordinates></Polygon>
-#     </Placemark></kml>"""
-#     fake_root = ET.fromstring(kml_content)
+    mock_json_load.return_value = [{"id": "MALFORMED"}]
 
-#     # Configure the mock tree to return our fake root
-#     mock_tree = mock.MagicMock()
-#     mock_tree.getroot.return_value = fake_root
-#     mock_et_parse.return_value = mock_tree
+    result = import_aggressor_port_data()
 
-#     result = import_kml_data()
-
-#     assert result is True
-#     mock_db_session.return_value.__enter__.return_value.add.assert_called_once()
-
-#     added_obj = mock_db_session.return_value.__enter__.return_value.add.call_args[0][0]
-
-#     assert added_obj.cocom_type == CocomType.EUCOM
-#     mock_db_session.return_value.__enter__.return_value.commit.assert_called_once()
-
-
-# @mock.patch("oms_sensemaking.core.cocom.check_geodata", return_value=False)
-# @mock.patch("oms_sensemaking.core.cocom.ET.parse")
-# @mock.patch("oms_sensemaking.core.cocom.db_session")
-# def test_import_kml_data_error(mock_db_session, mock_et_parse, mock_check_geodata):
-
-#     mock_et_parse.side_effect = Exception("Malformed XML or File not found")
-
-#     result = import_kml_data()
-
-#     assert result is False
-#     mock_db_session.return_value.__enter__.return_value.rollback.assert_called()
+    assert result is False
+    mock_db_session.return_value.__enter__.return_value.add.assert_not_called()
+    mock_db_session.return_value.__enter__.return_value.rollback.assert_called()
